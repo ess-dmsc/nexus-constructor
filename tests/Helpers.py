6b820fde695054fa9720b048e7225b37d79c97c7
Alex Lay <alexander.lay@stfc.ac.uk>
2018-09-25 16:54:04 +0100

    #8 Clean up code style in test helpers

--- tests/Helpers.py
+++ tests/Helpers.py
@@ -6,7 +6,7 @@ from PySide2.QtTest import QTest
 
 # pytest-qt's qtbot.mouseClick() only works directly on QtWidget's elements, not qml's.
 # Fortunately, the method it calls down to has an overload
-# that takes a position and a QWindow, which QML is compatible with.
+# that takes a position and a QWindow, which QML can be compatible with.
 def click_object(object: QQuickItem, parentWindow: QWindow):
     point = object.mapToScene(QtCore.QPoint(0, 0)).toPoint()
     point.setX(point.x() + (object.width() / 2))
@@ -19,7 +19,7 @@ def tree_search_items(item: QQuickItem, targetobjectname: str):
     for child in item.childItems():
         if child.objectName() == targetobjectname:
             return child
-        next = tree_search_items(child, targetobjectname)
-        if next is not None:
-            return next
+        childresult = tree_search_items(child, targetobjectname)
+        if childresult is not None:
+            return childresult
     return None
