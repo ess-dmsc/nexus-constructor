2d6923bd2a5d29d9234e8ecac4dae0d0e6ae746d
Alex Lay <alexander.lay@stfc.ac.uk>
2018-10-04 14:07:56 +0100

    Remove QtWidgets dependency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from os import path
-from PySide2.QtWidgets import QApplication
+from PySide2.QtGui import QGuiApplication
 from geometry_constructor.Window import Window
 
 
@@ -8,7 +8,7 @@ sys_argv = sys.argv + ['--style', 'Material']
 location = sys.executable if getattr(sys, 'frozen', False) else __file__
 resource_folder = path.join(path.dirname(location), 'resources')
 
-app = QApplication(sys_argv)
+app = QGuiApplication(sys_argv)
 view = Window(resource_folder)
 view.show()
 res = app.exec_()
