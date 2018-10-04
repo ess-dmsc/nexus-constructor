4cb145a37e2e1547c9d30dc233f28a0adcd0e8e5
Alex Lay <alexander.lay@stfc.ac.uk>
2018-10-04 14:09:46 +0100

    Rename packaged executable

--- setup.py
+++ setup.py
@@ -13,12 +13,15 @@ build_exe_options = {"packages": ["numpy.core._methods",
                      "include_files": ["resources"]}
 
 # GUI applications require a different base on Windows (the default is for a console application).
-base = None
 if sys.platform == "win32":
     base = "Win32GUI"
+    extension = '.exe'
+else:
+    base = None
+    extension = ''
 
 setup(name="Nexus Geometry Test App",
       version="0.1",
       description="Technology test program for the nexus geometry constructor",
       options={"build_exe": build_exe_options},
-      executables=[Executable("main.py", base=base)])
+      executables=[Executable("main.py", base=base, targetName="NexusGeometry" + extension)])
