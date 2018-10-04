da7e720c6d3f4d33287ef7d0d10d36c79ae0c6d1
Alex Lay <alexander.lay@stfc.ac.uk>
2018-10-04 17:24:22 +0100

    Move application style definition to qrc and conf files

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import sys
 from os import path
 from PySide2.QtGui import QGuiApplication
+from geometry_constructor import resources  # NOQA
 from geometry_constructor.Window import Window
 
-
-sys_argv = sys.argv + ['--style', 'Material']
 location = sys.executable if getattr(sys, 'frozen', False) else __file__
 resource_folder = path.join(path.dirname(location), 'resources')
 
-app = QGuiApplication(sys_argv)
+app = QGuiApplication(sys.argv)
 view = Window(resource_folder)
 view.show()
 res = app.exec_()
