1847bfadf6e7cb2f3401f2f3d9b9dbafe7fece92
Alex Lay <alexander.lay@stfc.ac.uk>
2018-10-10 12:06:15 +0100

    Add test for generated hdf5 grid coordinates

--- tests/test_hdf_writer.py
+++ tests/test_hdf_writer.py
@@ -3,6 +3,31 @@ from geometry_constructor.Writers import HdfWriter
 from geometry_constructor.Models import InstrumentModel, Detector, PixelGrid, Corner, CountDirection
 
 
+def test_save_pixel_grid_coordinates():
+    instrument = InstrumentModel()
+    instrument.components.append(Detector(name='detector',
+                                          id=1,
+                                          transform_parent_id=0,
+                                          pixel_data=PixelGrid(rows=3, columns=4, row_height=1.5, col_width=2,
+                                                               first_id=0, count_direction=CountDirection.ROW,
+                                                               initial_count_corner=Corner.BOTTOM_LEFT)))
+    # use an in memory file to avoid disk usage during tests
+    with h5py.File('grid_coordinate_testfile', driver='core', backing_store=False) as file:
+        HdfWriter().save_instrument_to_file(file, instrument)
+        x_dataset = file['/entry/instrument/detector/x_pixel_offset'][::].tolist()
+        y_dataset = file['/entry/instrument/detector/y_pixel_offset'][::].tolist()
+        z_dataset = file['/entry/instrument/detector/z_pixel_offset'][::].tolist()
+        assert x_dataset == [[0, 2, 4, 6],
+                             [0, 2, 4, 6],
+                             [0, 2, 4, 6]]
+        assert y_dataset == [[0, 0, 0, 0],
+                             [1.5, 1.5, 1.5, 1.5],
+                             [3, 3, 3, 3]]
+        assert z_dataset == [[0, 0, 0, 0],
+                             [0, 0, 0, 0],
+                             [0, 0, 0, 0]]
+
+
 def assess_pixel_grid_direction(direction, corner, expected_data):
     instrument = InstrumentModel()
     instrument.components.append(Detector(name='detector',
