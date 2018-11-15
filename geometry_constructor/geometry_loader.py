8c00061afce5fe4784c561d0eb7dd5b60b6bd262
Alex Lay <alexander.lay@stfc.ac.uk>
2018-11-15 10:26:55 +0000

    Add docstrings to geometry loader methods

--- geometry_constructor/geometry_loader.py
+++ geometry_constructor/geometry_loader.py
@@ -4,6 +4,14 @@ from stl import mesh
 
 
 def load_geometry(filename: str):
+    """
+    Loads geometry from a file into an OFFGeometry instance
+
+    Supported file types are OFF and STL.
+    :param filename: The name of the file to load geometry from
+    :return: An OFFGeometry instance containing that file's geometry, or an empty instance if filename's extension is
+    unsupported
+    """
     extension = filename[filename.rfind('.'):].lower()
     if extension == '.off':
         return load_off_geometry(filename)
@@ -15,6 +23,12 @@ def load_geometry(filename: str):
 
 
 def load_off_geometry(filename: str):
+    """
+    Loads geometry from an OFF file into an OFFGeometry instance
+
+    :param filename: The name of the OFF file to load geometry from
+    :return: An OFFGeometry instance containing that file's geometry
+    """
     with open(filename) as file:
         vertices, faces = parse_off_file(file)
 
@@ -25,6 +39,12 @@ def load_off_geometry(filename: str):
 
 
 def load_stl_geometry(filename: str):
+    """
+    Loads geometry from an STL file into an OFFGeometry instance
+
+    :param filename: The name of the STL file to load geometry from
+    :return: An OFFGeometry instance containing that file's geometry
+    """
     mesh_data = mesh.Mesh.from_file(filename, calculate_normals=False)
     # numpy-stl loads numbers as python decimals, not floats, which aren't valid in json
     vertices = [Vector(float(corner[0]),
