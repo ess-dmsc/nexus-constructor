4b3518c537858768227a2c414137f7036e39ca1c
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-02-11 15:45:33 +0000

    Testing valid JSON without file loading

--- tests/test_json_connector.py
+++ tests/test_json_connector.py
@@ -6,10 +6,20 @@ def test_valid_json_returns_true():
 
     json_connector = JsonConnector()
 
-    with open("tests/sample.json", mode="r") as file:
-        json_string = file.read()
+    valid_json = (r'{'
+                  r'"components": [],'
+                  r'"sample": {'
+                  r'"geometry": {'
+                  r'"type": "OFF",'
+                  r'"vertices": ['
+                  r'[-0.5, -0.5, 0.5]'
+                  r']'
+                  r'},'
+                  r'"type": "Sample"'
+                  r'}'
+                  r'}')
 
-    assert json_connector.json_string_to_instrument_model(json_string, model())
+    assert json_connector.json_string_to_instrument_model(valid_json, model())
 
 
 def test_invalid_jsons_returns_false():
