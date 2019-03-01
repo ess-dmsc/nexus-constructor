574a3df1395665389cf57a3e7e54d4aaabd63cc1
Jack Harper <jack.harper@stfc.ac.uk>
2019-03-01 17:27:17 +0000

    adding test_off_renderer, refactoring off_renderer

--- tests/test_qml_json_model.py
+++ tests/test_qml_json_model.py
@@ -8,7 +8,7 @@ def read_json():
     """
     with open("tests/sample.json", mode="r") as file:
         data = file.read()
-    with open("tests/formatted sample.json", mode="r") as file:
+    with open("tests/formattedsample.json", mode="r") as file:
         formatted_lines = file.read().splitlines()
     return data, formatted_lines
 
@@ -18,7 +18,7 @@ def read_collapsed_data():
     Reads the expected collapsed lines for a model based on the sample json data
     :return: a list of strings containing the collapsed lines
     """
-    with open("tests/collapsed lines.txt") as file:
+    with open("tests/collapsedlines.txt") as file:
         return file.read().splitlines()
 
 
