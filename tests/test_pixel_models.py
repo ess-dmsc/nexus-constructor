4b6fb6e0c24ad961702407c9a6b9ee45757b8c92
Jack Harper <jack.harper@stfc.ac.uk>
2019-04-23 09:32:38 +0100

    review changes

--- tests/test_pixel_models.py
+++ tests/test_pixel_models.py
@@ -1,3 +1,4 @@
+from nexus_constructor.data_model import ComponentType
 from nexus_constructor.qml_models.pixel_models import (
     PixelGridModel,
     PixelMappingModel,
@@ -23,20 +24,23 @@ def test_GIVEN_pixel_id_WHEN_creating_single_pixel_model_THEN_pixel_id_is_set_in
 
 def test_GIVEN_pixel_model_WHEN_creating_pixelgridmodel_THEN_model_is_updated():
     pixel_data = PixelGrid(rows=5)
-    model = PixelGridModel()
     inst = InstrumentModel()
-    inst.components[0].pixel_data = pixel_data
-    model.set_pixel_model(0, inst)
+    model = PixelGridModel()
+    inst.add_component(ComponentType.DETECTOR, "test", pixel_model=model)
+    inst.components[-1].pixel_data = pixel_data
+    model.set_pixel_model(len(inst.components) - 1, inst)
 
     assert pixel_data == model.get_pixel_model()
 
 
-def test_GIVEN_another_pixel_id_object_WHEN_setting_model_THEN_model_is_replaced():
+def test_GIVEN_single_pixel_id_WHEN_setting_model_THEN_model_is_replaced():
     pixel_data = SinglePixelId(5)
-    model = SinglePixelModel()
     inst = InstrumentModel()
-    inst.components[0].pixel_data = pixel_data
-    model.set_pixel_model(0, inst)
+    model = SinglePixelModel()
+
+    inst.add_component(ComponentType.DETECTOR, "test", pixel_model=model)
+    inst.components[-1].pixel_data = pixel_data
+    model.set_pixel_model(len(inst.components) - 1, inst)
 
     assert pixel_data == model.get_pixel_model()
 
@@ -51,10 +55,11 @@ def test_GIVEN_columns_WHEN_setting_pixel_data_columns_in_pixelgridmodel_THEN_mo
 def test_GIVEN_pixel_object_WHEN_setting_pixel_model_on_mapping_model_THEN_model_is_updated_with_new_object():
     pixel_data = PixelMapping([1])
     inst = InstrumentModel()
-    inst.components[0].pixel_data = pixel_data
     model = PixelMappingModel()
+    inst.add_component(ComponentType.DETECTOR, "test", pixel_model=model)
+    inst.components[-1].pixel_data = pixel_data
 
-    model.set_pixel_model(0, inst)
+    model.set_pixel_model(len(inst.components) - 1, inst)
 
     assert model.get_pixel_model() == pixel_data
 
@@ -75,28 +80,29 @@ def test_GIVEN_nothing_WHEN_creating_PixelGridModel_THEN_pixeldata_type_is_pixel
 
 
 def test_GIVEN_same_pixelid_WHEN_setting_data_on_pixelMappingModel_THEN_returns_changed_as_false():
-    id = 1
-    component_index = 0
-    pixel_data = PixelMapping([id])
+
     inst = InstrumentModel()
-    inst.components[component_index].pixel_data = pixel_data
     model = PixelMappingModel()
+    inst.add_component(ComponentType.DETECTOR, "test", pixel_model=model)
+    component_index = len(inst.components) - 1
+    id = 1
+    pixel_data = PixelMapping([id])
+    inst.components[component_index].pixel_data = pixel_data
     model.set_pixel_model(component_index, inst)
 
     assert not model.setData(inst.index(component_index), id, model.PixelIdRole)
 
 
 def test_GIVEN_different_pixelid_WHEN_setting_data_on_pixelMappingModel_THEN_model_is_updated():
-    component_index = 0
     original_id = 0
     pixel_data = PixelMapping([original_id])
     inst = InstrumentModel()
-    inst.components[component_index].pixel_data = pixel_data
     model = PixelMappingModel()
+    inst.add_component(ComponentType.DETECTOR, "test", pixel_model=model)
+    component_index = len(inst.components) - 1
+    inst.components[component_index].pixel_data = pixel_data
     model.set_pixel_model(component_index, inst)
 
     changed_id = 1
-
     assert model.setData(inst.index(component_index), changed_id, model.PixelIdRole)
-
     assert model.data(inst.index(component_index), model.PixelIdRole) == changed_id
