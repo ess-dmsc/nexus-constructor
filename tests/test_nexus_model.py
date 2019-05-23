9cb8242390e5799e09fb9cb588a8f6e271044e78
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-05-23 12:07:23 +0100

    Make test more self-documentng

--- tests/test_nexus_model.py
+++ tests/test_nexus_model.py
@@ -92,7 +92,7 @@ def test_GIVEN_unrecognised_name_WHEN_deleting_group_THEN_throws():
         delete_group("nonexistent_component_name", nexus_file)
 
 
-def test_GIVEN_repeated_name_WHEN_creating_group_that_shares_its_name_with_deleted_component_THEN_creation_successful():
+def test_GIVEN_repeated_name_WHEN_creating_group_that_shares_its_name_with_deleted_group_THEN_group_creation_successful():
     file_name = "test4"
     nx_class = "NXarbitrary"
     component_name = "MyDetector"
