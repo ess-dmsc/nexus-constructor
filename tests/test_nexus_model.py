497766276e34e94dc35d150bc43a2a217518e731
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-05-23 10:10:35 +0100

    Test for return value of create_group

--- tests/test_nexus_model.py
+++ tests/test_nexus_model.py
@@ -99,7 +99,8 @@ def test_GIVEN_repeated_name_WHEN_creating_component_that_shares_its_name_with_d
     nexus_file = h5py.File(file_name, driver="core", backing_store=False)
     create_group(component_name, nx_class, nexus_file)
     delete_group(component_name, nexus_file)
-    create_group(component_name, nx_class, nexus_file)
+
+    assert isinstance(create_group(component_name, nx_class, nexus_file), h5py._hl.group.Group)
 
 
 def test_GIVEN_string_ending_with_nxs_WHEN_appending_nxs_extension_THEN_string_is_not_changed():
