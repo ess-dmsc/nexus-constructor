e56e15e168e8ab2b229ccaffc6cf90f58221c93a
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-06-03 15:33:03 +0100

    Created transformation matrix for cylinder

--- ui/InstrumentView.py
+++ ui/InstrumentView.py
@@ -1,7 +1,8 @@
 from PySide2.QtWidgets import QWidget, QVBoxLayout
 from PySide2.Qt3DExtras import Qt3DExtras
 from PySide2.Qt3DCore import Qt3DCore
-from PySide2.QtGui import QVector3D, QColor
+from PySide2.QtGui import QVector3D, QColor, QMatrix4x4
+
 
 class InstrumentView(QWidget):
     def __init__(self):
@@ -22,7 +23,6 @@ class InstrumentView(QWidget):
         camController.setCamera(cameraEntity)
         self.view.setRootEntity(self.rootEntity)
 
-        self.create_materials()
         self.initialise_view()
 
     def create_materials(self):
@@ -42,7 +42,16 @@ class InstrumentView(QWidget):
         self.red_material.setAmbient(red)
         self.red_material.setDiffuse(darkred)
 
-    def initialise_view(self):
+        self.beam_material = Qt3DExtras.QPhongAlphaMaterial()
+        self.beam_material.setAmbient(blue)
+        self.beam_material.setDiffuse(lightblue)
+        self.beam_material.setAlpha(0.5)
+
+        self.green_material = Qt3DExtras.QPhongMaterial()
+        self.green_material.setAmbient(grey)
+        self.green_material.setDiffuse(grey)
+
+    def create_sample_cube(self):
 
         self.cubeEntity = Qt3DCore.QEntity(self.rootEntity)
         self.cubeMesh = Qt3DExtras.QCuboidMesh()
@@ -50,13 +59,30 @@ class InstrumentView(QWidget):
         self.cubeMesh.setYExtent(5)
         self.cubeMesh.setZExtent(5)
 
-        '''
-        self.sphereTransform = Qt3DCore.QTransform()
-        self.sphereTransform.setTranslation(QVector3D(x, y, z))
-        '''
-
         self.cubeEntity.addComponent(self.cubeMesh)
         self.cubeEntity.addComponent(self.red_material)
-        # self.cubeEntity.addComponent(self.sphereTransform)
 
-        print("Created cube.")
+    def create_beam_cylinder(self):
+
+        self.cylinderEntity = Qt3DCore.QEntity(self.rootEntity)
+        self.cylinderMesh = Qt3DExtras.QCylinderMesh()
+        self.cylinderMesh.setRadius(2.5)
+        self.cylinderMesh.setLength(40)
+        self.cylinderMesh.setRings(2)
+
+        cylinderMatrix = QMatrix4x4()
+        cylinderMatrix.rotate(270, QVector3D(1, 0, 0))
+        cylinderMatrix.translate(QVector3D(0, 20, 0))
+
+        self.cylinderTransform = Qt3DCore.QTransform()
+        self.cylinderTransform.setMatrix(cylinderMatrix)
+
+        self.cylinderEntity.addComponent(self.cylinderMesh)
+        self.cylinderEntity.addComponent(self.beam_material)
+        self.cylinderEntity.addComponent(self.cylinderTransform)
+
+    def initialise_view(self):
+
+        self.create_materials()
+        self.create_sample_cube()
+        self.create_beam_cylinder()
