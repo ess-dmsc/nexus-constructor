4a2de36edff5472bcac68e5f697610a3f0a1b609
Jack Harper <jack.harper@stfc.ac.uk>
2019-06-07 15:07:41 +0100

    sorting out setup.py

--- setup.py
+++ setup.py
@@ -31,20 +31,12 @@ build_exe_options = {
 }
 
 unix_removable = [
-    "lib/PySide2/Qt/lib/libQt5WebEngine.so.5",
-    "lib/PySide2/Qt/lib/libQt5WebEngineCore.so.5",
-    "lib/PySide2/Qt/lib/libQt5WebEngineWidgets.so.5",
-    "lib/PySide2/QtWidgets.abi3.so",
     "lib/PySide2/libclang.so.6",
     "lib/PySide2/Qt/resources/",
     "lib/PySide2/Qt/translations/",
 ]
 
 win_removable = [
-    "lib/PySide2/Qt5WebEngine.dll",
-    "lib/PySide2/Qt5WebEngineCore.dll",
-    "lib/PySide2/Qt5WebEngineWidgets.dll",
-    "lib/PySide2/QtWidgets.pyd",
     "lib/PySide2/libclang.dll",
     "lib/PySide2/resources/",
     "lib/PySide2/translations/",
