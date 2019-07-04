8eaa3b44403f405bfb6e660848e07201d5dc6248
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-07-04 14:31:43 +0100

    Moving InstrumentView tests

---
+++ tests/test_qentity_utils.py
@@ -0,0 +1,59 @@
+from mock import Mock, call
+
+from nexus_constructor.qentity_utils import (
+    set_material_properties,
+    add_qcomponents_to_entity,
+)
+
+
+def test_GIVEN_material_properties_WHEN_calling_set_material_properties_THEN_properties_set():
+
+    ambient = Mock()
+    diffuse = Mock()
+
+    mock_material = Mock()
+    mock_material.setAmbient = Mock()
+    mock_material.setDiffuse = Mock()
+
+    # This method doesn't exist for QPhongMaterial but is mocked all the same to make sure that it isn't called when an
+    # alpha argument isn't given
+    mock_material.setAlpha = Mock()
+
+    mock_material.setShininess = Mock()
+
+    set_material_properties(mock_material, ambient, diffuse)
+
+    mock_material.setAmbient.assert_called_once_with(ambient)
+    mock_material.setDiffuse.assert_called_once_with(diffuse)
+    mock_material.setAlpha.assert_not_called()
+    mock_material.setShininess.assert_not_called()
+
+
+def test_GIVEN_alpha_material_properties_WHEN_calling_set_material_properties_THEN_properties_set():
+
+    ambient = Mock()
+    diffuse = Mock()
+    alpha = 0.5
+
+    mock_alpha_material = Mock()
+    mock_alpha_material.setAmbient = Mock()
+    mock_alpha_material.setDiffuse = Mock()
+    mock_alpha_material.setAmbient = Mock()
+
+    set_material_properties(mock_alpha_material, ambient, diffuse, alpha)
+
+    mock_alpha_material.setAmbient.assert_called_once_with(ambient)
+    mock_alpha_material.setDiffuse.assert_called_once_with(diffuse)
+    mock_alpha_material.setAlpha.assert_called_once_with(alpha)
+
+
+def test_GIVEN_components_WHEN_calling_add_components_to_entity_THEN_components_added():
+
+    mock_entity = Mock()
+    mock_entity.addComponent = Mock()
+    mock_components = [Mock() for _ in range(4)]
+    calls = [call(mock_component) for mock_component in mock_components]
+
+    add_qcomponents_to_entity(mock_entity, mock_components)
+
+    mock_entity.addComponent.assert_has_calls(calls)
