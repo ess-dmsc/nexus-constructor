5d3ef860a70010277ac46596508547d7d6de4856
Matthew D Jones <matthew.d.jones@tessella.com>
2019-07-04 09:12:02 +0100

    #334 Add tests for get_an_orthogonal_unit_vector

--- tests/test_geometry_utils.py
+++ tests/test_geometry_utils.py
@@ -3,7 +3,7 @@ from nexus_constructor.geometry.utils import (
     validate_nonzero_qvector,
 )
 from PySide2.QtGui import QVector3D
-from pytest import raises, fail
+from pytest import raises, fail, approx
 
 
 def test_zero_qvector_raises_error_when_validated():
@@ -18,3 +18,18 @@ def test_non_zero_qvector_does_not_raise_error_when_validated():
         validate_nonzero_qvector(test_vector)
     except ValueError:
         fail("Expected non-zero vector to pass validation")
+
+
+def test_get_an_orthogonal_unit_vector_gives_a_unit_vector():
+    test_vector = QVector3D(2, 6, 3)
+    result_vector = get_an_orthogonal_unit_vector(test_vector)
+    assert result_vector.length() == approx(1)
+
+
+def test_get_an_orthogonal_unit_vector_gives_an_orthogonal_vector():
+    test_vector = QVector3D(2, 6, 3)
+    result_vector = get_an_orthogonal_unit_vector(test_vector)
+    print(test_vector)
+    print(result_vector)
+    # Test orthogonal (dot product is zero)
+    assert QVector3D.dotProduct(test_vector, result_vector) == approx(0)
