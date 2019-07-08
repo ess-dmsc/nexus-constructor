793a07bb3afbf9e206d534781d525e3a83d7b04d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-07-08 15:49:08 +0100

    Comments for LineGeometry class

--- nexus_constructor/line_geometry.py
+++ nexus_constructor/line_geometry.py
@@ -10,18 +10,24 @@ class LineGeometry(Qt3DRender.QGeometry):
         :param parent: The object parent.
         """
         Qt3DRender.QGeometry.__init__(self, parent)
-        self.m_position_buffer = Qt3DRender.QBuffer(self)
-        self.m_position_buffer.setUsage(Qt3DRender.QBuffer.StaticDraw)
-        self.m_position_attribute = Qt3DRender.QAttribute(self)
-        self.m_position_attribute.setAttributeType(
-            Qt3DRender.QAttribute.VertexAttribute
-        )
-        self.m_position_attribute.setVertexBaseType(Qt3DRender.QAttribute.Float)
-        self.m_position_attribute.setVertexSize(3)
-        self.m_position_attribute.setName(
+
+        # Create a position buffer and give it the vertex data
+        self.position_buffer = Qt3DRender.QBuffer(self)
+        self.position_buffer.setUsage(Qt3DRender.QBuffer.StaticDraw)
+        self.position_buffer.setData(vertex_data)
+
+        # Configure a position attribute and configure it to represent a set of coordinates
+        self.position_attribute = Qt3DRender.QAttribute(self)
+        self.position_attribute.setAttributeType(Qt3DRender.QAttribute.VertexAttribute)
+        self.position_attribute.setVertexBaseType(Qt3DRender.QAttribute.Float)
+        self.position_attribute.setVertexSize(3)
+        self.position_attribute.setName(
             Qt3DRender.QAttribute.defaultPositionAttributeName()
         )
-        self.m_position_attribute.setBuffer(self.m_position_buffer)
-        self.addAttribute(self.m_position_attribute)
-        self.m_position_buffer.setData(vertex_data)
-        self.m_position_attribute.setCount(2)
+        self.position_attribute.setBuffer(self.position_buffer)
+
+        # Set the number of points contained in the attribute
+        # This must be two for the start and end of the line
+        self.position_attribute.setCount(2)
+
+        self.addAttribute(self.position_attribute)
