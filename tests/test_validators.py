3433c2b7f678e514c5f218c8e16204a96e6945cf
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-07-09 12:22:05 +0100

    Checking for validation emitting True

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -138,8 +138,24 @@ def test_GIVEN_invalid_units_WHEN_using_ok_validator_with_no_geometry_button_unc
     validator.set_units_valid(False)
 
 
-def test_GIVEN_invalid_file_WHEN_using_ok_validator_with_mesh_option_button_THEN_false_signal_is_emitted():
+def test_GIVEN_invalid_file_WHEN_using_ok_validator_with_mesh_option_button_checked_THEN_false_signal_is_emitted():
 
     validator, mock_mesh_button, mock_no_geometry_button = create_content_ok_validator()
     validator.is_valid.connect(lambda x: inspect_signal(x, expected=False))
+    validator.set_file_valid(False)
+
+
+def test_GIVEN_invalid_units_WHEN_using_ok_validator_WITH_no_geometry_button_checked_THEN_true_signal_is_emitted():
+
+    validator, mock_mesh_button, mock_no_geometry_button = create_content_ok_validator()
+    mock_no_geometry_button.isChecked = Mock(return_value=True)
+    validator.is_valid.connect(lambda x: inspect_signal(x, expected=True))
+    validator.set_units_valid(False)
+
+
+def test_GIVEN_invalid_file_WHEN_using_ok_validator_WITH_mesh_button_unchecked_THEN_true_signal_is_emitted():
+
+    validator, mock_mesh_button, mock_no_geometry_button = create_content_ok_validator()
+    mock_mesh_button.isChecked = Mock(return_value=False)
+    validator.is_valid.connect(lambda x: inspect_signal(x, expected=True))
     validator.set_file_valid(False)
