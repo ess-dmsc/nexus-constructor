085ff6ba6ac33c925a50ff3b97d81a13d2525aaf
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-07-10 14:31:15 +0100

    Testing appearance of pixel options

--- tests/ui_tests/test_ui_add_component_window.py
+++ tests/ui_tests/test_ui_add_component_window.py
@@ -2,6 +2,7 @@ import pytest
 from PySide2.QtCore import Qt
 from PySide2.QtWidgets import QMainWindow, QDialog
 
+from nexus_constructor import component_type
 from nexus_constructor.add_component_window import AddComponentDialog
 from nexus_constructor.component_tree_model import ComponentTreeModel
 from nexus_constructor.instrument import Instrument
@@ -12,19 +13,21 @@ from nexus_constructor.nexus.nexus_wrapper import NexusWrapper
 # Workaround - even when skipping jenkins is not happy importing AddComponentDialog due to a missing lib
 
 
-@pytest.mark.skip(reason="Leads to seg faults. Clicking with QActions is not possible.")
+@pytest.mark.skip(
+    reason="Leads to seg faults. Clicking with QActions/QIcons doesn't seem to be possible."
+)
 def test_UI_GIVEN_nothing_WHEN_clicking_add_component_button_THEN_add_component_window_is_shown(
     qtbot
 ):
     template = QMainWindow()
-    window = MainWindow(Instrument(NexusWrapper("test1")))
+    window = MainWindow(Instrument(NexusWrapper("test")))
     template.ui = window
     template.ui.setupUi(template)
 
     qtbot.addWidget(template)
 
     # Using trigger rather than clicking on the menu
-    window.new_component_action.trigger()
+    template.show()
     assert window.add_component_window.isVisible()
 
     window.add_component_window.close()
@@ -33,10 +36,8 @@ def test_UI_GIVEN_nothing_WHEN_clicking_add_component_button_THEN_add_component_
 def test_UI_GIVEN_no_geometry_WHEN_selecting_geometry_type_THEN_geometry_options_are_hidden(
     qtbot
 ):
-
-    instrument = Instrument(NexusWrapper("test2"))
     template = QDialog()
-    dialog = AddComponentDialog(instrument, ComponentTreeModel(instrument))
+    dialog = create_add_component_dialog(1)
     template.ui = dialog
     template.ui.setupUi(template)
 
@@ -50,10 +51,8 @@ def test_UI_GIVEN_no_geometry_WHEN_selecting_geometry_type_THEN_geometry_options
 def test_UI_GIVEN_cylinder_geometry_WHEN_selecting_geometry_type_THEN_relevant_fields_are_shown(
     qtbot
 ):
-    instrument = Instrument(NexusWrapper("test3"))
-    component = ComponentTreeModel(instrument)
     template = QDialog()
-    dialog = AddComponentDialog(instrument, component)
+    dialog = create_add_component_dialog(2)
     template.ui = dialog
     template.ui.setupUi(template)
 
@@ -62,6 +61,7 @@ def test_UI_GIVEN_cylinder_geometry_WHEN_selecting_geometry_type_THEN_relevant_f
     qtbot.mouseClick(dialog.CylinderRadioButton, Qt.LeftButton)
 
     template.show()
+    qtbot.waitForWindowShown(template)
     assert dialog.geometryOptionsBox.isVisible()
     assert dialog.cylinderOptionsBox.isVisible()
     assert dialog.unitsbox.isVisible()
@@ -70,11 +70,8 @@ def test_UI_GIVEN_cylinder_geometry_WHEN_selecting_geometry_type_THEN_relevant_f
 def test_GIVEN_mesh_geometry_WHEN_selecting_geometry_type_THEN_relevant_fields_are_shown(
     qtbot
 ):
-
-    instrument = Instrument(NexusWrapper("test4"))
-    component = ComponentTreeModel(instrument)
     template = QDialog()
-    dialog = AddComponentDialog(instrument, component)
+    dialog = create_add_component_dialog(3)
     template.ui = dialog
     template.ui.setupUi(template)
 
@@ -83,6 +80,42 @@ def test_GIVEN_mesh_geometry_WHEN_selecting_geometry_type_THEN_relevant_fields_a
     qtbot.mouseClick(dialog.meshRadioButton, Qt.LeftButton)
 
     template.show()
+    qtbot.waitForWindowShown(template)
     assert dialog.geometryOptionsBox.isVisible()
     assert dialog.unitsbox.isVisible()
     assert dialog.geometryFileBox.isVisible()
+
+
+def test_GIVEN_correct_class_WHEN_selecting_nxclass_THEN_pixel_options_becomes_visible(
+    qtbot
+):
+    template = QDialog()
+    dialog = create_add_component_dialog(4)
+    template.ui = dialog
+    template.ui.setupUi(template)
+
+    classes = list(dialog.nx_component_classes.keys())
+    pixel_options_class_indices = []
+
+    for i, nx_class in enumerate(classes):
+        if nx_class in component_type.PIXEL_COMPONENT_TYPES:
+            pixel_options_class_indices.append(i)
+
+    pixel_geometry_buttons = [dialog.meshRadioButton, dialog.CylinderRadioButton]
+
+    for geometry_button in pixel_geometry_buttons:
+
+        qtbot.mouseClick(geometry_button, Qt.LeftButton)
+
+        for index in pixel_options_class_indices:
+            dialog.componentTypeComboBox.setCurrentIndex(index)
+            template.show()
+            qtbot.waitForWindowShown(template)
+            assert dialog.pixelOptionsBox.isVisible()
+
+
+def create_add_component_dialog(test_count):
+    nexus_name = "test" + str(test_count)
+    instrument = Instrument(NexusWrapper(nexus_name))
+    component = ComponentTreeModel(instrument)
+    return AddComponentDialog(instrument, component)
