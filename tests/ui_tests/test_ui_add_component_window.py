10d43f17ba9f3c8bdb582014d4f14d671e558027
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-07-11 10:00:20 +0100

    Test that add component window closes or remains opening depending on if input was valid/add component button was enabled

--- tests/ui_tests/test_ui_add_component_window.py
+++ tests/ui_tests/test_ui_add_component_window.py
@@ -9,8 +9,11 @@ from nexus_constructor.instrument import Instrument
 from nexus_constructor.main_window import MainWindow
 from nexus_constructor.nexus.nexus_wrapper import NexusWrapper
 
-
 # Workaround - even when skipping jenkins is not happy importing AddComponentDialog due to a missing lib
+
+nexus_wrapper_count = 0
+RED_BACKGROUND_STYLE_SHEET = "QLineEdit { background-color: #f6989d }"
+WHITE_BACKGROUND_STYLE_SHEET = "QLineEdit { background-color: #FFFFFF }"
 
 
 @pytest.mark.skip(
@@ -37,7 +40,7 @@ def test_UI_GIVEN_no_geometry_WHEN_selecting_geometry_type_THEN_geometry_options
     qtbot
 ):
     template = QDialog()
-    dialog = create_add_component_dialog(1)
+    dialog = create_add_component_dialog()
     template.ui = dialog
     template.ui.setupUi(template)
 
@@ -52,7 +55,7 @@ def test_UI_GIVEN_cylinder_geometry_WHEN_selecting_geometry_type_THEN_relevant_f
     qtbot
 ):
     template = QDialog()
-    dialog = create_add_component_dialog(2)
+    dialog = create_add_component_dialog()
     template.ui = dialog
     template.ui.setupUi(template)
 
@@ -78,7 +81,7 @@ def test_GIVEN_mesh_geometry_WHEN_selecting_geometry_type_THEN_relevant_fields_a
     qtbot
 ):
     template = QDialog()
-    dialog = create_add_component_dialog(3)
+    dialog = create_add_component_dialog()
     template.ui = dialog
     template.ui.setupUi(template)
 
@@ -104,7 +107,7 @@ def test_GIVEN_class_with_pixel_fields_WHEN_selecting_nxclass_THEN_pixel_options
     qtbot
 ):
     template = QDialog()
-    dialog = create_add_component_dialog(4)
+    dialog = create_add_component_dialog()
     template.ui = dialog
     template.ui.setupUi(template)
 
@@ -133,7 +136,7 @@ def test_GIVEN_class_without_pixel_fields_WHEN_selecting_nxclass_THEN_pixel_opti
 ):
 
     template = QDialog()
-    dialog = create_add_component_dialog(5)
+    dialog = create_add_component_dialog()
     template.ui = dialog
     template.ui.setupUi(template)
 
@@ -179,21 +182,21 @@ def test_GIVEN_valid_name_WHEN_choosing_component_name_THEN_background_becomes_w
 ):
 
     template = QDialog()
-    dialog = create_add_component_dialog(6)
+    dialog = create_add_component_dialog()
     template.ui = dialog
     template.ui.setupUi(template)
 
     qtbot.addWidget(template)
 
     # Check that the background color of the ext field starts as red
-    assert dialog.nameLineEdit.styleSheet() == "QLineEdit { background-color: #f6989d }"
+    assert dialog.nameLineEdit.styleSheet() == RED_BACKGROUND_STYLE_SHEET
 
     # Mimic the user entering a name in the text field
     qtbot.mouseClick(dialog.nameLineEdit, Qt.LeftButton)
     qtbot.keyClicks(dialog.nameLineEdit, "AUniqueName")
 
     # Check that the background color of the test field has changed to white
-    assert dialog.nameLineEdit.styleSheet() == "QLineEdit { background-color: #FFFFFF }"
+    assert dialog.nameLineEdit.styleSheet() == WHITE_BACKGROUND_STYLE_SHEET
 
 
 def test_GIVEN_repeated_name_WHEN_choosing_component_name_THEN_background_remains_red(
@@ -201,27 +204,71 @@ def test_GIVEN_repeated_name_WHEN_choosing_component_name_THEN_background_remain
 ):
 
     template = QDialog()
-    dialog = create_add_component_dialog(7)
+    dialog = create_add_component_dialog()
     template.ui = dialog
     template.ui.setupUi(template)
 
     qtbot.addWidget(template)
 
-    red_background_style_sheet = "QLineEdit { background-color: #f6989d }"
-
     # Check that the background color of the text field starts as red
-    assert dialog.nameLineEdit.styleSheet() == red_background_style_sheet
+    assert dialog.nameLineEdit.styleSheet() == RED_BACKGROUND_STYLE_SHEET
 
     # Mimic the user entering a non-unique name in the text field
     qtbot.mouseClick(dialog.nameLineEdit, Qt.LeftButton)
     qtbot.keyClicks(dialog.nameLineEdit, "sample")
 
     # Check that the background color of the test field has remained red
-    assert dialog.nameLineEdit.styleSheet() == red_background_style_sheet
+    assert dialog.nameLineEdit.styleSheet() == RED_BACKGROUND_STYLE_SHEET
 
 
-def create_add_component_dialog(test_count):
-    nexus_name = "test" + str(test_count)
+def test_GIVEN_invalid_input_WHEN_adding_component_THEN_add_component_window_remains_open(
+    qtbot
+):
+
+    template = QDialog()
+    dialog = create_add_component_dialog()
+    template.ui = dialog
+    template.ui.setupUi(template)
+
+    qtbot.addWidget(template)
+
+    template.show()
+    qtbot.waitForWindowShown(template)
+
+    # Mimic the user entering a non-unique name in the text field
+    qtbot.mouseClick(dialog.nameLineEdit, Qt.LeftButton)
+    qtbot.keyClicks(dialog.nameLineEdit, "sample")
+    qtbot.mouseClick(dialog.buttonBox, Qt.LeftButton)
+
+    assert template.isVisible()
+
+
+def test_GIVEN_valid_input_WHEN_adding_component_THEN_add_component_window_closes(
+    qtbot
+):
+
+    template = QDialog()
+    dialog = create_add_component_dialog()
+    template.ui = dialog
+    template.ui.setupUi(template)
+
+    qtbot.addWidget(template)
+
+    template.show()
+    qtbot.waitForWindowShown(template)
+
+    # Mimic the user entering a non-unique name in the text field
+    qtbot.mouseClick(dialog.nameLineEdit, Qt.LeftButton)
+    qtbot.keyClicks(dialog.nameLineEdit, "AUniqueName")
+    qtbot.mouseClick(dialog.buttonBox, Qt.LeftButton)
+
+    assert not template.isVisible()
+
+
+def create_add_component_dialog():
+    global nexus_wrapper_count
+    nexus_name = "test" + str(nexus_wrapper_count)
     instrument = Instrument(NexusWrapper(nexus_name))
     component = ComponentTreeModel(instrument)
+    nexus_wrapper_count += 1
     return AddComponentDialog(instrument, component)
