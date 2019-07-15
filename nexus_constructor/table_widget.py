a1c7db93fe15e0be4fbd81af41eb76e36fb4b0ea
Jack Harper <jack.harper@stfc.ac.uk>
2019-07-15 10:49:18 +0100

    adding delete row functionality

--- nexus_constructor/table_widget.py
+++ nexus_constructor/table_widget.py
@@ -1,36 +1,44 @@
 import numpy as np
 import typing
 from PySide2.QtCore import QModelIndex, QAbstractTableModel, Qt
-from PySide2.QtWidgets import QWidget, QGridLayout, QTableView, QToolBar, QAction
+from PySide2.QtWidgets import (
+    QWidget,
+    QGridLayout,
+    QTableView,
+    QToolBar,
+    QAction,
+    QAbstractItemView,
+)
 
 
 class TableWidget(QWidget):
     def __init__(self, type: np.dtype = np.byte, parent=None):
         super().__init__(parent)
         self.model = TableModel(dtype=type, parent=self)
+        self.view = QTableView()
+        self.view.setModel(self.model)
+        self.view.setSelectionMode(QAbstractItemView.SingleSelection)
+
         self.setLayout(QGridLayout())
-
         self.toolbox = QToolBar()
-
         self.add_row_button = QAction(text="➕ Add Row")
         self.add_row_button.triggered.connect(self.model.add_row)
         self.remove_row_button = QAction(text="➖ Remove Row")
+        self.remove_row_button.triggered.connect(self.model.delete_row, self)
         self.add_column_button = QAction(text="➕ Add Column")
         self.add_column_button.triggered.connect(self.model.add_column)
         self.remove_column_button = QAction(text="➖ Remove Column")
-
+        self.remove_column_button.triggered.connect(self.model.delete_column, self)
         buttons = [
             self.add_row_button,
             self.remove_row_button,
             self.add_column_button,
             self.remove_column_button,
         ]
-
         [self.toolbox.addAction(button) for button in buttons]
 
         self.layout().addWidget(self.toolbox)
-        self.view = QTableView()
-        self.view.setModel(self.model)
+
         self.layout().addWidget(self.view)
 
 
@@ -49,16 +57,27 @@ class TableModel(QAbstractTableModel):
     def add_column(self):
         pass
 
-    def delete_row(self, selected_indexes: QModelIndex):
-        pass
+    def delete_row(self, parent):
+        for index in parent.selectedIndexes():
+            np.delete(self.array, index.row())
 
-    def delete_column(self, selected_indexes: QModelIndex):
+    def delete_column(self, parent):
         pass
 
     def rowCount(self, parent: QModelIndex = ...) -> int:
+        """
+        Dictates how many rows there are in the table.
+        :param parent: Unused.
+        :return: Number of elements in each dimension.
+        """
         return self.array.shape[0]
 
     def columnCount(self, parent: QModelIndex = ...) -> int:
+        """
+        Dictates how many columns there are in the table.
+        :param parent: Unused.
+        :return: Number of dimensions there are in the array.
+        """
         return self.array.shape[1]
 
     def data(self, index: QModelIndex, role: int = ...) -> typing.Any:
@@ -69,12 +88,17 @@ class TableModel(QAbstractTableModel):
     def flags(self, index: QModelIndex) -> Qt.ItemFlags:
         if not index.isValid():
             return Qt.ItemIsEnabled
-        return QAbstractTableModel.flags(self, index) | Qt.ItemIsEditable
+        return (
+            QAbstractTableModel.flags(self, index)
+            | Qt.ItemIsEditable
+            | Qt.ItemIsSelectable
+            | Qt.ItemIsEnabled
+        )
 
     def headerData(
         self, section: int, orientation: Qt.Orientation, role: int = ...
     ) -> typing.Any:
-        if role == Qt.DisplayRole:
+        if role == Qt.DisplayRole or role == Qt.EditRole:
             if orientation == Qt.Vertical:
                 return f"{section:d}"
             if orientation == Qt.Horizontal:
@@ -82,7 +106,7 @@ class TableModel(QAbstractTableModel):
         return None
 
     def setData(self, index: QModelIndex, value: typing.Any, role: int = ...) -> bool:
-        if index.isValid() and role == Qt.EditRole:
+        if index.isValid() and role == Qt.EditRole and value:
             self.array[index.row()][index.column()] = value
             self.dataChanged.emit(index, index)
             return True
