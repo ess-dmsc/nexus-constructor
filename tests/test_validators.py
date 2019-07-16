d08e8f2a7a0d068ca812a58e788470da2af33dda
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-07-16 09:23:40 +0100

    Extra NullableIntValidator test

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -278,3 +278,9 @@ def test_GIVEN_nonemptry_string_WHEN_using_nullable_int_validator_THEN_returns_i
 
     for invalid_input in not_integers:
         assert validator.validate(invalid_input, 0)[0] == QValidator.State.Invalid
+
+
+def test_GIVEN_integer_WHEN_using_nullable_int_validator_THEN_returns_valid():
+
+    validator = NullableIntValidator()
+    assert validator.validate("5", 0) == QValidator.Valid
