08548567421774b7710ba09da1fc50cc7600de34
Jack Harper <jack.harper@stfc.ac.uk>
2019-07-22 15:51:13 +0100

    stopping user from reducing array to less than (1,1)

--- nexus_constructor/table_widget.py
+++ nexus_constructor/table_widget.py
@@ -94,7 +94,11 @@ class TableModel(QAbstractTableModel):
         """
         self.beginResetModel()
         for index in self.parent().view.selectedIndexes():
-            if self.array.shape != (1, 1):
+            if self.array.size > 0:
+                if is_row and self.array.shape[0] <= 1:
+                    return
+                elif not is_row and self.array.shape[1] <= 1:
+                    return
                 self.array = np.delete(
                     self.array,
                     (index.row() if is_row else index.column()),
