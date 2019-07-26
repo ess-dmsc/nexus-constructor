4f0883504ae59bf41187ba5a1afc31b6b820d2e2
Jack Harper <jack.harper@stfc.ac.uk>
2019-07-26 17:06:15 +0100

    adding f142 stuff

--- nexus_constructor/stream_fields_widget.py
+++ nexus_constructor/stream_fields_widget.py
@@ -1,6 +1,7 @@
-from PySide2.QtWidgets import QComboBox, QGridLayout, QLineEdit, QDialog
+from PySide2.QtWidgets import QComboBox, QGridLayout, QLineEdit, QDialog, QSpinBox
 
 SCHEMAS = ["ev42", "f142", "hs00", ""]
+types = ["double", "int"]
 
 
 class StreamFieldsWidget(QDialog):
@@ -12,6 +13,12 @@ class StreamFieldsWidget(QDialog):
         self.schema_combo.currentTextChanged.connect(self.schema_type_changed)
         self.schema_combo.addItems(SCHEMAS)
         self.topic_line_edit = QLineEdit()
+        self.source_line_edit = QLineEdit()
+
+        self.type_combo = QComboBox()
+        self.type_combo.currentTextChanged.connect(self._type_changed)
+
+        self.array_size_spinbox = QSpinBox()
 
         self.layout().addWidget(self.schema_combo)
         self.layout().addWidget(self.topic_line_edit)
@@ -20,3 +27,12 @@ class StreamFieldsWidget(QDialog):
 
     def schema_type_changed(self, schema):
         self.parent().setWindowTitle(f"Editing {schema} stream field")
+        if schema == "f142":
+            self._set_edits_enabled(True, True)
+
+    def _set_edits_enabled(self, source: bool, type: bool):
+        self.source_line_edit.setEnabled(source)
+        self.type_combo.setEnabled(type)
+
+    def _type_changed(self, type):
+        self.array_size_spinbox.setEnabled(type == "double")
