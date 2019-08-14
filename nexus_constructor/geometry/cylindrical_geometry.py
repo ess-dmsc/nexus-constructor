32deac4bc5814946e425ffff7d2019d3e31678cc
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-08-14 13:47:15 +0100

    Consistent imports

--- nexus_constructor/geometry/cylindrical_geometry.py
+++ nexus_constructor/geometry/cylindrical_geometry.py
@@ -1,25 +1,24 @@
-from math import sin, cos, pi, acos, degrees
-from typing import Tuple, List
-
-import h5py
-import numpy as np
 from PySide2.QtGui import QVector3D, QMatrix4x4
 
-from nexus_constructor.geometry.off_geometry import OFFGeometry, OFFGeometryNoNexus
-from nexus_constructor.geometry.utils import get_an_orthogonal_unit_vector
+from nexus_constructor.pixel_data import PixelMapping
+from nexus_constructor.pixel_data_to_nexus_utils import detector_number
+from nexus_constructor.unit_converter import calculate_unit_conversion_factor
+from math import sin, cos, pi, acos, degrees
+import h5py
+import numpy as np
 from nexus_constructor.nexus import nexus_wrapper as nx
 from nexus_constructor.nexus.validation import (
     NexusFormatError,
     ValidateDataset,
     validate_group,
 )
-from nexus_constructor.pixel_data import PixelMapping
-from nexus_constructor.pixel_data_to_nexus_utils import detector_number
 from nexus_constructor.ui_utils import (
     numpy_array_to_qvector3d,
     qvector3d_to_numpy_array,
 )
-from nexus_constructor.unit_converter import calculate_unit_conversion_factor
+from nexus_constructor.geometry.utils import get_an_orthogonal_unit_vector
+from nexus_constructor.geometry.off_geometry import OFFGeometry, OFFGeometryNoNexus
+from typing import Tuple, List
 
 
 def calculate_vertices(
