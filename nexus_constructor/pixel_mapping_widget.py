3b96b96e576070c40390413c3dc26786b40dcc80
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-08-14 12:52:10 +0100

    Type hints for PixelMappingWidget

--- nexus_constructor/pixel_mapping_widget.py
+++ nexus_constructor/pixel_mapping_widget.py
@@ -1,21 +1,23 @@
-from PySide2.QtWidgets import QWidget, QLabel, QLineEdit, QHBoxLayout
+from PySide2.QtWidgets import QWidget, QLabel, QLineEdit, QHBoxLayout, QListWidget
 
 from nexus_constructor.validators import NullableIntValidator
 
 
 class PixelMappingWidget(QWidget):
-    def __init__(self, parent, face_no, text):
+    def __init__(self, parent: QListWidget, id_no: int, text: str):
         """
         A custom widget used for acting as a template in the pixel mapping list. Consists of a label and an input box.
         :param parent: The QListWidget that will contain this list item.
-        :param face_no: The face number from the geometry file.
+        :param id_no: The "temporary" ID number based on the number of faces in the mesh/number of cylinders. Used
+        to distinguish elements in the list but doesn't define the actual IDs that are saved upon clicking Add Component.
+        :param text: The text that appears in the list widget. Will be either "cylinder" or "face."
         """
         super().__init__(parent)
         self.pixelIDLabel = QLabel()
         self.pixelIDLineEdit = QLineEdit()
 
         # Create the label text based on the face number
-        self.pixelIDLabel.setText("Pixel ID for " + text + " #" + str(face_no) + ":")
+        self.pixelIDLabel.setText("Pixel ID for " + text + " #" + str(id_no) + ":")
 
         # Give the line edit a validator that requires values of zero or greater
         self.pixelIDLineEdit.setValidator(NullableIntValidator(bottom=0))
