7099ab32a1a226d8e12acb76e44f0f9e10eaf84b
Jack Harper <jack.harper@stfc.ac.uk>
2019-10-07 14:58:27 +0100

    correcting display format for time

--- nexus_constructor/filewriter_command_dialog.py
+++ nexus_constructor/filewriter_command_dialog.py
@@ -1,3 +1,4 @@
+from PySide2.QtCore import QDateTime
 from PySide2.QtWidgets import (
     QDialog,
     QFormLayout,
@@ -7,6 +8,8 @@ from PySide2.QtWidgets import (
     QPushButton,
 )
 
+TIME_FORMAT = "yyyy MM dd hh:mm:ss"
+
 
 class FilewriterCommandDialog(QDialog):
     def __init__(self):
@@ -15,15 +18,24 @@ class FilewriterCommandDialog(QDialog):
         self.setLayout(QFormLayout())
 
         self.nexus_file_name_edit = QLineEdit()
-        self.start_time_picker = QDateTimeEdit()
-        self.stop_time_picker = QDateTimeEdit()
+
+        self.start_time_enabled = QCheckBox()
+        self.start_time_picker = QDateTimeEdit(QDateTime.currentDateTime())
+        self.start_time_picker.setDisplayFormat(TIME_FORMAT)
+
+        self.stop_time_enabled = QCheckBox()
+        self.stop_time_picker = QDateTimeEdit(QDateTime.currentDateTime())
+        self.stop_time_picker.setDisplayFormat(TIME_FORMAT)
+
         self.service_id_lineedit = QLineEdit()
         self.abort_on_unitialised_stream_checkbox = QCheckBox()
         self.use_swmr_checkbox = QCheckBox()
         self.use_swmr_checkbox.setChecked(True)
 
         self.layout().addRow("nexus_file_name", self.nexus_file_name_edit)
+        self.layout().addRow("specify start time?", self.start_time_enabled)
         self.layout().addRow("start_time", self.start_time_picker)
+        self.layout().addRow("specify stop time?", self.stop_time_enabled)
         self.layout().addRow("stop_time", self.stop_time_picker)
         self.layout().addRow("service_id", self.service_id_lineedit)
         self.layout().addRow(
@@ -31,7 +43,7 @@ class FilewriterCommandDialog(QDialog):
         )
         self.layout().addRow("use_hdf_swmr", self.use_swmr_checkbox)
 
-        self.ok_button = QPushButton()
+        self.ok_button = QPushButton("Ok")
         self.ok_button.clicked.connect(self.close)
         self.layout().addRow(self.ok_button)
 
