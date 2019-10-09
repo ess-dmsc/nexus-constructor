2be184e31a6d15871fa0b2838e3239d6d5dc0930
Jack Harper <jack.harper@stfc.ac.uk>
2019-10-09 10:43:20 +0100

    renaming and tidying

--- ui/main_window.py
+++ ui/main_window.py
@@ -7,79 +7,91 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1287, 712)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.splitter = QSplitter(self.centralwidget)
+        self.central_widget = QtWidgets.QWidget(MainWindow)
+        self.central_widget.setObjectName("centralwidget")
+
+        self.splitter = QSplitter(self.central_widget)
         self.splitter.setChildrenCollapsible(False)
-        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.centralwidget)
-        self.verticalLayout_3.setObjectName("verticalLayout_3")
-        self.gridLayout_3 = QtWidgets.QGridLayout()
-        self.gridLayout_3.addWidget(self.splitter)
-        self.gridLayout_3.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
-        self.gridLayout_3.setObjectName("gridLayout_3")
-        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
-        self.tabWidget.setMinimumSize(QtCore.QSize(500, 0))
-        self.tabWidget.setObjectName("tabWidget")
-        self.tab_2 = QtWidgets.QWidget()
-        self.tab_2.setObjectName("tab_2")
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.tab_2)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
+
+        self.main_grid_layout = QtWidgets.QGridLayout(self.central_widget)
+        self.main_grid_layout.addWidget(self.splitter)
+        self.main_grid_layout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.main_grid_layout.setObjectName("gridLayout_3")
+
+        self.tab_widget = QtWidgets.QTabWidget(self.central_widget)
+        self.tab_widget.setMinimumSize(QtCore.QSize(500, 0))
+        self.tab_widget.setObjectName("tabWidget")
+        self.component_tree_view_tab = QtWidgets.QWidget()
+        self.component_tree_view_tab.setObjectName("tab_2")
+
+        self.tree_view_layout = QtWidgets.QVBoxLayout(self.component_tree_view_tab)
+        self.tree_view_layout.setObjectName("verticalLayout_2")
+
         self.componentsTabLayout = QtWidgets.QVBoxLayout()
         self.componentsTabLayout.setObjectName("componentsTabLayout")
-        self.componentTreeView = QtWidgets.QTreeView(self.tab_2)
-        self.componentTreeView.setObjectName("componentTreeView")
-        self.componentsTabLayout.addWidget(self.componentTreeView)
-        self.verticalLayout_2.addLayout(self.componentsTabLayout)
-        self.tabWidget.addTab(self.tab_2, "")
-        self.tab = QtWidgets.QWidget()
-        self.tab.setObjectName("tab")
-        self.gridLayout_2 = QtWidgets.QGridLayout(self.tab)
-        self.gridLayout_2.setObjectName("gridLayout_2")
-        self.verticalLayout = QtWidgets.QVBoxLayout()
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.widget = QtWidgets.QWidget(self.tab)
-        self.widget.setObjectName("widget")
-        self.verticalLayout.addWidget(self.widget)
-        self.gridLayout_2.addLayout(self.verticalLayout, 0, 0, 1, 1)
-        self.tabWidget.addTab(self.tab, "")
-        self.splitter.addWidget(self.tabWidget)
+
+        self.component_tree_view = QtWidgets.QTreeView(self.component_tree_view_tab)
+        self.component_tree_view.setObjectName("componentTreeView")
+        self.componentsTabLayout.addWidget(self.component_tree_view)
+        self.tree_view_layout.addLayout(self.componentsTabLayout)
+
+        self.tab_widget.addTab(self.component_tree_view_tab, "")
+        self.silx_tab = QtWidgets.QWidget()
+        self.silx_tab.setObjectName("tab")
+
+        self.silx_tab_layout = QtWidgets.QGridLayout(self.silx_tab)
+        self.silx_tab_layout.setObjectName("gridLayout_2")
+
+        self.silx_vertical_layout = QtWidgets.QVBoxLayout()
+        self.silx_vertical_layout.setObjectName("verticalLayout")
+
+        self.silx_widget = QtWidgets.QWidget(self.silx_tab)
+        self.silx_widget.setObjectName("widget")
+
+        self.silx_vertical_layout.addWidget(self.silx_widget)
+        self.silx_tab_layout.addLayout(self.silx_vertical_layout, 0, 0, 1, 1)
+
+        self.tab_widget.addTab(self.silx_tab, "")
+        self.splitter.addWidget(self.tab_widget)
+
         self.sceneWidget = InstrumentView(self.splitter)
-        sizePolicy = QtWidgets.QSizePolicy(
+        size_policy = QtWidgets.QSizePolicy(
             QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding
         )
-        sizePolicy.setHorizontalStretch(1)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.sceneWidget.sizePolicy().hasHeightForWidth())
-        self.sceneWidget.setSizePolicy(sizePolicy)
+        size_policy.setHorizontalStretch(1)
+        size_policy.setVerticalStretch(0)
+        size_policy.setHeightForWidth(self.sceneWidget.sizePolicy().hasHeightForWidth())
+        self.sceneWidget.setSizePolicy(size_policy)
         self.sceneWidget.setMinimumSize(QtCore.QSize(745, 0))
         self.sceneWidget.setObjectName("sceneWidget")
         self.splitter.addWidget(self.sceneWidget)
-        self.verticalLayout_3.addLayout(self.gridLayout_3)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar()
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1287, 22))
-        self.menubar.setObjectName("menubar")
-        self.menuFile = QtWidgets.QMenu(self.menubar)
-        self.menuFile.setObjectName("menuFile")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        self.actionOpen_NeXus_file = QtWidgets.QAction(MainWindow)
-        self.actionOpen_NeXus_file.setObjectName("actionOpen_NeXus_file")
-        self.actionExport_to_NeXus_file = QtWidgets.QAction(MainWindow)
-        self.actionExport_to_NeXus_file.setObjectName("actionExport_to_NeXus_file")
-        self.actionExport_to_Filewriter_JSON = QtWidgets.QAction(MainWindow)
-        self.actionExport_to_Filewriter_JSON.setObjectName(
+
+        MainWindow.setCentralWidget(self.central_widget)
+
+        self.menu_bar = QtWidgets.QMenuBar()
+        self.menu_bar.setGeometry(QtCore.QRect(0, 0, 1287, 22))
+        self.menu_bar.setObjectName("menubar")
+        self.file_menu = QtWidgets.QMenu(self.menu_bar)
+        self.file_menu.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menu_bar)
+        self.status_bar = QtWidgets.QStatusBar(MainWindow)
+        self.status_bar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.status_bar)
+        self.open_nexus_file_action = QtWidgets.QAction(MainWindow)
+        self.open_nexus_file_action.setObjectName("actionOpen_NeXus_file")
+        self.export_to_nexus_file_action = QtWidgets.QAction(MainWindow)
+        self.export_to_nexus_file_action.setObjectName("actionExport_to_NeXus_file")
+        self.export_to_filewriter_JSON_action = QtWidgets.QAction(MainWindow)
+        self.export_to_filewriter_JSON_action.setObjectName(
             "actionExport_to_Filewriter_JSON"
         )
-        self.menuFile.addAction(self.actionOpen_NeXus_file)
-        self.menuFile.addAction(self.actionExport_to_NeXus_file)
-        self.menuFile.addAction(self.actionExport_to_Filewriter_JSON)
-        self.menubar.addAction(self.menuFile.menuAction())
+        self.file_menu.addAction(self.open_nexus_file_action)
+        self.file_menu.addAction(self.export_to_nexus_file_action)
+        self.file_menu.addAction(self.export_to_filewriter_JSON_action)
+        self.menu_bar.addAction(self.file_menu.menuAction())
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(0)
+        self.tab_widget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -88,28 +100,28 @@ class Ui_MainWindow(object):
                 "MainWindow", "NeXus Constructor", None, -1
             )
         )
-        self.tabWidget.setTabText(
-            self.tabWidget.indexOf(self.tab_2),
+        self.tab_widget.setTabText(
+            self.tab_widget.indexOf(self.component_tree_view_tab),
             QtWidgets.QApplication.translate("MainWindow", "Components", None, -1),
         )
-        self.tabWidget.setTabText(
-            self.tabWidget.indexOf(self.tab),
+        self.tab_widget.setTabText(
+            self.tab_widget.indexOf(self.silx_tab),
             QtWidgets.QApplication.translate(
                 "MainWindow", "NeXus File Layout", None, -1
             ),
         )
-        self.menuFile.setTitle(
+        self.file_menu.setTitle(
             QtWidgets.QApplication.translate("MainWindow", "File", None, -1)
         )
-        self.actionOpen_NeXus_file.setText(
+        self.open_nexus_file_action.setText(
             QtWidgets.QApplication.translate("MainWindow", "Open NeXus file", None, -1)
         )
-        self.actionExport_to_NeXus_file.setText(
+        self.export_to_nexus_file_action.setText(
             QtWidgets.QApplication.translate(
                 "MainWindow", "Export to NeXus file", None, -1
             )
         )
-        self.actionExport_to_Filewriter_JSON.setText(
+        self.export_to_filewriter_JSON_action.setText(
             QtWidgets.QApplication.translate(
                 "MainWindow", "Export to Filewriter JSON", None, -1
             )
