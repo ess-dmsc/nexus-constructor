8650b9b4467d83f139b8e7ea80d094ef76796b58
Matthew D Jones <matthew.d.jones@tessella.com>
2019-10-09 14:26:13 +0100

    Revert validation, better done with JSON schema in separate ticket

--- nexus_constructor/nexus_filewriter_json/reader.py
+++ nexus_constructor/nexus_filewriter_json/reader.py
@@ -1,66 +1,13 @@
 import json
-from typing import Tuple, List, Union
 
 from nexus_constructor.nexus.nexus_wrapper import NexusWrapper
 
 """
-Read the JSON, perform some validation and construct an in-memory NeXus file from the nexus_structure field
+Read the JSON and construct an in-memory NeXus file from the nexus_structure field
 """
 
 
-def _validate_broker(broker: Union[str, object]):
-    pass
-
-
-def _validate_job_id(job_id: Union[str, object]):
-    pass
-
-
-def _validate_file_attributes(file_attributes: Union[str, object]):
-    pass
-
-
-def _validate_nexus_structure(nexus_structure: Union[str, object]):
-    pass
-
-
-def _validate_command(command: Union[str, object]):
-    allowed_commands = {"FileWriter_new"}
-    if command not in allowed_commands:
-        return '"cmd" field contains invalid string'
-
-
-REQUIRED_FIELDS = {
-    "cmd": _validate_command,
-    "broker": _validate_broker,
-    "job_id": _validate_job_id,
-    "file_attributes": _validate_file_attributes,
-    "nexus_structure": _validate_nexus_structure,
-}
-
-
-def validate_top_level_fields(json_data: dict) -> List[str]:
-    """
-    Checks required fields are present in the json and returns warning if not
-    :param json_data:
-    :return: A list of warning messages created during validation
-    """
-    warning_messages = []
-
-    for required_field, validator in REQUIRED_FIELDS.items():
-        try:
-            field_value = json_data[required_field]
-        except KeyError:
-            warning_messages.append(f'Required field "{required_field}" is missing')
-        else:
-            message = validator(field_value)
-            if message is not None:
-                warning_messages.append(message)
-
-    return warning_messages
-
-
-def json_to_nexus(json_input: str) -> Tuple[NexusWrapper, List[str]]:
+def json_to_nexus(json_input: str) -> NexusWrapper:
     """
     Convert JSON to in-memory NeXus file
     :param json_input:
@@ -71,8 +18,7 @@ def json_to_nexus(json_input: str) -> Tuple[NexusWrapper, List[str]]:
         raise ValueError("Empty json string, nothing to load!")
 
     json_data = json.loads(json_input)
-    validation_output = validate_top_level_fields(json_data)
 
     wrapper = NexusWrapper("json_to_nexus")
 
-    return wrapper, validation_output
+    return wrapper
--- tests/test_json_reader.py
+++ tests/test_json_reader.py
@@ -1,10 +1,6 @@
 import pytest
 import json
-from nexus_constructor.nexus_filewriter_json.reader import (
-    json_to_nexus,
-    validate_top_level_fields,
-    REQUIRED_FIELDS,
-)
+from nexus_constructor.nexus_filewriter_json.reader import json_to_nexus
 
 
 def test_GIVEN_empty_json_string_WHEN_json_to_nexus_called_THEN_error_is_raised():
@@ -15,46 +11,3 @@ def test_GIVEN_empty_json_string_WHEN_json_to_nexus_called_THEN_error_is_raised(
 def test_GIVEN_invalid_json_string_WHEN_json_to_nexus_called_THEN_error_is_raised():
     with pytest.raises(ValueError):
         json_to_nexus("{")
-
-
-def test_GIVEN_json_with_missing_required_fields_WHEN_validated_THEN_message_returned_for_each_missing_field():
-    json_input = r"{}"
-    json_data = json.loads(json_input)
-    messages = validate_top_level_fields(json_data)
-    assert len(messages) == len(
-        REQUIRED_FIELDS
-    ), "Expect same number of warning messages as missing required fields"
-
-
-def test_GIVEN_json_with_valid_required_fields_WHEN_validated_THEN_no_messages_returned():
-    json_input = (
-        r"{"
-        r'"cmd": "FileWriter_new",'
-        r'"broker": "localhost:9092",'
-        r'"job_id": "test_job",'
-        r'"file_attributes": {"file_name": "test_file.nxs"},'
-        r'"nexus_structure": {"children": []}'
-        r"}"
-    )
-    json_data = json.loads(json_input)
-    messages = validate_top_level_fields(json_data)
-    assert (
-        len(messages) == 0
-    ), "Expect no messages if all required fields are present and valid"
-
-
-def test_GIVEN_json_with_invalid_required_fields_WHEN_validated_THEN_message_returned_for_each_invalid_field():
-    json_input = (
-        r"{"
-        r'"cmd": "INVALID_CMD",'
-        r'"broker": "localhost:9092",'
-        r'"job_id": "test_job",'
-        r'"file_attributes": "INVALID_FILE_ATTRS",'
-        r'"nexus_structure": "INVALID_NEXUS_STRUCTURE"'
-        r"}"
-    )
-    json_data = json.loads(json_input)
-    messages = validate_top_level_fields(json_data)
-    assert (
-        len(messages) == 3
-    ), "Expect no messages if all required fields are present and valid"
