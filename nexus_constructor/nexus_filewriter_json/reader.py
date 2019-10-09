63e7a7aaa2c9f22faeac46e9bc30d64277c3c8ad
Matthew D Jones <matthew.d.jones@tessella.com>
2019-10-09 12:31:35 +0100

    Impl empty json results in error

--- nexus_constructor/nexus_filewriter_json/reader.py
+++ nexus_constructor/nexus_filewriter_json/reader.py
@@ -5,7 +5,10 @@ Read the JSON and construct an in-memory NeXus file from it
 """
 
 
-def json_to_nexus(json_input: str):
+def json_to_nexus(json_input: str) -> NexusWrapper:
+    if not json_input:
+        raise ValueError("Empty json string, nothing to load!")
+
     wrapper = NexusWrapper("json_to_nexus")
 
-    # Go through json, top down
+    return wrapper
