f50cb688afb7b9617c398955ffff895c832a153f
Jack Harper <jack.harper@stfc.ac.uk>
2019-10-10 10:35:01 +0100

    refactoring out pixel options group box (in progress)

--- ui/pixel_options.py
+++ ui/pixel_options.py
@@ -56,6 +56,30 @@ class Ui_PixelOptionsWidget(object):
         self.pixel_grid_page.setObjectName("pixelGridPage")
         self.pixel_grid_page_layout = QtWidgets.QVBoxLayout(self.pixel_grid_page)
         self.pixel_grid_page_layout.setObjectName("verticalLayout")
+
+        self._set_up_pixel_grid_group_box()
+
+        self.pixel_options_stack.addWidget(self.pixel_grid_page)
+        self.pixel_mapping_page = QtWidgets.QWidget()
+        self.pixel_mapping_page.setObjectName("pixelMappingPage")
+        self.pixel_mapping_page_layout = QtWidgets.QVBoxLayout(self.pixel_mapping_page)
+        self.pixel_mapping_page_layout.setObjectName("verticalLayout_4")
+        self.pixel_mapping_label = QtWidgets.QLabel(self.pixel_mapping_page)
+        self.pixel_mapping_label.setObjectName("pixelMappingLabel")
+        self.pixel_mapping_page_layout.addWidget(self.pixel_mapping_label)
+        self.pixel_mapping_list_widget = QtWidgets.QListWidget(self.pixel_mapping_page)
+        self.pixel_mapping_list_widget.setObjectName("pixelMappingListWidget")
+        self.pixel_mapping_page_layout.addWidget(self.pixel_mapping_list_widget)
+        self.pixel_options_stack.addWidget(self.pixel_mapping_page)
+        self.group_box_layout.addWidget(self.pixel_options_stack)
+        self.main_layout.addWidget(self.pixel_options_group_box)
+
+        self.retranslateUi(PixelOptionsWidget)
+        self.pixel_options_stack.setCurrentIndex(0)
+        self.count_first_combo_box.setCurrentIndex(-1)
+        QtCore.QMetaObject.connectSlotsByName(PixelOptionsWidget)
+
+    def _set_up_pixel_grid_group_box(self):
         self.pixel_grid_group_box = QtWidgets.QGroupBox(self.pixel_grid_page)
         self.pixel_grid_group_box.setObjectName("pixelGridBox")
         self.pixel_grid_group_box_layout = QtWidgets.QGridLayout(
@@ -84,7 +108,6 @@ class Ui_PixelOptionsWidget(object):
         self.counting_from_label = QtWidgets.QLabel(self.pixel_grid_group_box)
         self.counting_from_label.setObjectName("countingFromLabel")
         self.pixel_grid_group_box_layout.addWidget(self.counting_from_label, 3, 0, 1, 2)
-
         self.start_counting_combo_box = QtWidgets.QComboBox(self.pixel_grid_group_box)
         self.start_counting_combo_box.setMaxCount(4)
         self.start_counting_combo_box.setInsertPolicy(
@@ -98,7 +121,6 @@ class Ui_PixelOptionsWidget(object):
         self.pixel_grid_group_box_layout.addWidget(
             self.start_counting_combo_box, 3, 2, 1, 2
         )
-
         self.count_first_combo_box = QtWidgets.QComboBox(self.pixel_grid_group_box)
         self.count_first_combo_box.setCurrentText("")
         self.count_first_combo_box.setMaxCount(2)
@@ -107,14 +129,12 @@ class Ui_PixelOptionsWidget(object):
         self.pixel_grid_group_box_layout.addWidget(
             self.count_first_combo_box, 4, 2, 1, 2
         )
-
         self.row_count_spin_box = QtWidgets.QSpinBox(self.pixel_grid_group_box)
         self.row_count_spin_box.setMinimum(1)
         self.row_count_spin_box.setMaximum(1000000)
         self.row_count_spin_box.setProperty("value", 1)
         self.row_count_spin_box.setObjectName("rowCountSpinBox")
         self.pixel_grid_group_box_layout.addWidget(self.row_count_spin_box, 0, 1, 1, 1)
-
         self.column_count_spin_box = QtWidgets.QSpinBox(self.pixel_grid_group_box)
         self.column_count_spin_box.setMinimum(1)
         self.column_count_spin_box.setMaximum(1000000)
@@ -123,19 +143,16 @@ class Ui_PixelOptionsWidget(object):
         self.pixel_grid_group_box_layout.addWidget(
             self.column_count_spin_box, 1, 1, 1, 1
         )
-
         self.first_id_spin_box = QtWidgets.QSpinBox(self.pixel_grid_group_box)
         self.first_id_spin_box.setMaximum(1000000)
         self.first_id_spin_box.setObjectName("firstIDSpinBox")
         self.pixel_grid_group_box_layout.addWidget(self.first_id_spin_box, 2, 1, 1, 1)
-
         self.row_height_spin_box = QtWidgets.QDoubleSpinBox(self.pixel_grid_group_box)
         self.row_height_spin_box.setMinimum(0.1)
         self.row_height_spin_box.setSingleStep(0.1)
         self.row_height_spin_box.setProperty("value", 0.5)
         self.row_height_spin_box.setObjectName("rowHeightSpinBox")
         self.pixel_grid_group_box_layout.addWidget(self.row_height_spin_box, 0, 3, 1, 1)
-
         self.column_width_spin_box = QtWidgets.QDoubleSpinBox(self.pixel_grid_group_box)
         self.column_width_spin_box.setMinimum(0.1)
         self.column_width_spin_box.setSingleStep(0.1)
@@ -144,27 +161,7 @@ class Ui_PixelOptionsWidget(object):
         self.pixel_grid_group_box_layout.addWidget(
             self.column_width_spin_box, 1, 3, 1, 1
         )
-
         self.pixel_grid_page_layout.addWidget(self.pixel_grid_group_box)
-        self.pixel_options_stack.addWidget(self.pixel_grid_page)
-        self.pixel_mapping_page = QtWidgets.QWidget()
-        self.pixel_mapping_page.setObjectName("pixelMappingPage")
-        self.pixel_mapping_page_layout = QtWidgets.QVBoxLayout(self.pixel_mapping_page)
-        self.pixel_mapping_page_layout.setObjectName("verticalLayout_4")
-        self.pixel_mapping_label = QtWidgets.QLabel(self.pixel_mapping_page)
-        self.pixel_mapping_label.setObjectName("pixelMappingLabel")
-        self.pixel_mapping_page_layout.addWidget(self.pixel_mapping_label)
-        self.pixel_mapping_list_widget = QtWidgets.QListWidget(self.pixel_mapping_page)
-        self.pixel_mapping_list_widget.setObjectName("pixelMappingListWidget")
-        self.pixel_mapping_page_layout.addWidget(self.pixel_mapping_list_widget)
-        self.pixel_options_stack.addWidget(self.pixel_mapping_page)
-        self.group_box_layout.addWidget(self.pixel_options_stack)
-        self.main_layout.addWidget(self.pixel_options_group_box)
-
-        self.retranslateUi(PixelOptionsWidget)
-        self.pixel_options_stack.setCurrentIndex(0)
-        self.count_first_combo_box.setCurrentIndex(-1)
-        QtCore.QMetaObject.connectSlotsByName(PixelOptionsWidget)
 
     def retranslateUi(self, PixelOptionsWidget):
         PixelOptionsWidget.setWindowTitle(
