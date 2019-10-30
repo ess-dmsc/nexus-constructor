Fix direction of rotation transformations

tests/test_transformations.py
@@ -340,33 +340,30 @@ def test_can_get_translation_as_4_by_4_matrix():
 def test_can_get_rotation_as_4_by_4_matrix():
     nexus_wrapper = NexusWrapper(str(uuid1()))
 
-    test_value = np.pi / 2.0
+    test_value = 45.0  # degrees
     test_vector = QVector3D(0.0, 1.0, 0.0)  # around y-axis
     test_type = "Rotation"
     dataset = _add_transform_to_file(
-        nexus_wrapper,
-        "test_transform",
-        test_value * (180 / np.pi),
-        test_vector,
-        test_type,
+        nexus_wrapper, "test_transform", test_value, test_vector, test_type
     )
     transformation = Transformation(nexus_wrapper, dataset)
 
     test_matrix = transformation.qmatrix
     # for a rotation around the y-axis:
+    test_value_radians = np.deg2rad(test_value)
     expected_matrix = np.array(
         (
-            np.cos(test_value),
+            np.cos(-test_value_radians),
             0,
-            np.sin(test_value),
+            np.sin(-test_value_radians),
             0,
             0,
             1,
             0,
             0,
-            -np.sin(test_value),
+            -np.sin(-test_value_radians),
             0,
-            np.cos(test_value),
+            np.cos(-test_value_radians),
             0,
             0,
             0,