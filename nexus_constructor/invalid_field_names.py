8e63ec759732241521ed2c7019735260461eafe1
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-11-04 12:41:05 +0000

    preventing pixel info from appearing in fields widget

--- nexus_constructor/invalid_field_names.py
+++ nexus_constructor/invalid_field_names.py
@@ -2,4 +2,13 @@
 
 # These are invalid because there are separate inputs in the UI for these fields and therefore inputting them through
 # the field name line edit would cause conflicts.
-INVALID_FIELD_NAMES = ["description", "shape", "depends_on"]
+INVALID_FIELD_NAMES = [
+    "description",
+    "shape",
+    "depends_on",
+    "x_pixel_offset",
+    "y_pixel_offset",
+    "z_pixel_offset",
+    "detector_number",
+    "detector_faces",
+]
