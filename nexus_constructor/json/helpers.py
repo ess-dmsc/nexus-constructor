eddabdce11e006018cb57b4d3bf4fb7d8b26ef6d
Jonas Nilsson <jonas.nilsson@esss.se>
2019-11-05 16:38:24 +0100

    Fix type conversion.

--- nexus_constructor/json/helpers.py
+++ nexus_constructor/json/helpers.py
@@ -1,5 +1,12 @@
 import json
+import numpy as np
 
+def handle_non_std_types(Value):
+    if type(Value) is np.bool_:
+        return bool(Value)
+    elif type(Value) is np.ndarray:
+        return list(Value)
+    raise(TypeError("Unknown type: {}".format(type(Value))))
 
 def object_to_json_file(tree_dict, file):
     """
@@ -10,4 +17,4 @@ def object_to_json_file(tree_dict, file):
     :param file: File object to store the tree in.
     """
 
-    json.dump(tree_dict, file, indent=2, sort_keys=False)
+    json.dump(tree_dict, file, indent=2, sort_keys=False, default = handle_non_std_types)
