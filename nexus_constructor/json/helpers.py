fc752af97afed2c04ceed972a391dfea323fb172
Jonas Nilsson <jonas.nilsson@esss.se>
2019-11-05 16:40:32 +0100

    Format code.

--- nexus_constructor/json/helpers.py
+++ nexus_constructor/json/helpers.py
@@ -1,12 +1,14 @@
 import json
 import numpy as np
 
+
 def handle_non_std_types(Value):
     if type(Value) is np.bool_:
         return bool(Value)
     elif type(Value) is np.ndarray:
         return list(Value)
-    raise(TypeError("Unknown type: {}".format(type(Value))))
+    raise (TypeError("Unknown type: {}".format(type(Value))))
+
 
 def object_to_json_file(tree_dict, file):
     """
@@ -17,4 +19,4 @@ def object_to_json_file(tree_dict, file):
     :param file: File object to store the tree in.
     """
 
-    json.dump(tree_dict, file, indent=2, sort_keys=False, default = handle_non_std_types)
+    json.dump(tree_dict, file, indent=2, sort_keys=False, default=handle_non_std_types)
