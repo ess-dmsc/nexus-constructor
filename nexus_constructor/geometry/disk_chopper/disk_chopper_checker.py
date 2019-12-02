2cb768107baf60189b7d2418efb29079a5b4a505
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-12-02 09:51:15 +0000

    no reason to use set rather than list

--- nexus_constructor/geometry/disk_chopper/disk_chopper_checker.py
+++ nexus_constructor/geometry/disk_chopper/disk_chopper_checker.py
@@ -172,7 +172,7 @@ def _units_are_valid(units_dict: dict) -> bool:
     :param units_dict: The dictionary of units for the slit edges, radius, and slit height.
     :return: True if the units are all valid, False otherwise.
     """
-    bad_units = set()
+    bad_units = []
 
     for field in UNITS_REQUIRED:
         unit_input = units_dict[field]
@@ -181,19 +181,19 @@ def _units_are_valid(units_dict: dict) -> bool:
             logging.info(
                 f"{UNABLE} Units for {field} are not recognised. Found value: {unit_input}"
             )
-            bad_units.add(field)
+            bad_units.append(field)
             continue
         if not units_are_expected_type(unit_input, EXPECTED_UNIT_TYPE[field]):
             logging.info(
                 f"{UNABLE} Units for {field} have wrong type. Found {unit_input} but expected something that can be converted to {EXPECTED_UNIT_TYPE[field]}."
             )
-            bad_units.add(field)
+            bad_units.append(field)
             continue
         if not units_have_dimension_of_one(unit_input):
             logging.info(
                 f"{UNABLE} Units for {field} should habe a dimension of one. Found value: {unit_input}"
             )
-            bad_units.add(field)
+            bad_units.append(field)
             continue
 
     if len(bad_units) > 0:
