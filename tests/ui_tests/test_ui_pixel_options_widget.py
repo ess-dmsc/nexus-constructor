6a94456b5f33f6cd3303913e0b76cdafadf801e9
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-12-03 15:09:30 +0000

    xfail with reasons

--- tests/ui_tests/test_ui_pixel_options_widget.py
+++ tests/ui_tests/test_ui_pixel_options_widget.py
@@ -649,7 +649,9 @@ def test_GIVEN_pixel_grid_with_single_column_WHEN_finding_column_properties_THEN
     assert column_width is None
 
 
-@pytest.mark.xfail
+@pytest.mark.xfail(
+    "The function for recovering Pixel Grid information expects an array with more than one column/row. A single row of pixels isn't handled yet."
+)
 def test_GIVEN_row_of_pixels_WHEN_calling_get_detector_number_information_THEN_expected_start_counting_is_returned(
     pixel_options, pixel_grid
 ):
@@ -663,7 +665,9 @@ def test_GIVEN_row_of_pixels_WHEN_calling_get_detector_number_information_THEN_e
     assert start_counting_text in ["Right", "Left"]
 
 
-@pytest.mark.xfail
+@pytest.mark.xfail(
+    "The function for recovering Pixel Grid information expects an array with more than one column/row. A single column of pixels isn't handled yet."
+)
 def test_GIVEN_column_of_pixels_WHEN_calling_get_detector_number_information_THEN_expected_start_counting_is_returned(
     pixel_options, pixel_grid
 ):
