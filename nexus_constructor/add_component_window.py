98e2b51efe0fe0e114d9eeac4e172e7e3e5749ca
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-12-04 15:23:24 +0000

    chopper seems to be editable at least for non-arrays

--- nexus_constructor/add_component_window.py
+++ nexus_constructor/add_component_window.py
@@ -559,14 +559,17 @@ class AddComponentDialog(Ui_AddComponentDialog, QObject):
         :param pixel_data: The component PixelData. Can be None.
         :return: The geometry object.
         """
+        chopper_with_no_shape = self.component_is_chopper_with_no_shape(nx_class)
 
         # remove previous fields
         for field_group in self.component_to_edit.group.values():
             if field_group.name.split("/")[-1] not in INVALID_FIELD_NAMES:
                 del self.instrument.nexus.nexus_file[field_group.name]
 
-        # remove the previous shape from the qt3d view
-        if not isinstance(self.component_to_edit.shape[0], NoShapeGeometry):
+        # remove the previous object from the qt3d view
+        if chopper_with_no_shape or not isinstance(
+            self.component_to_edit.shape[0], NoShapeGeometry
+        ):
             self.parent().sceneWidget.delete_component(self.component_to_edit.name)
 
         self.component_to_edit.name = component_name
