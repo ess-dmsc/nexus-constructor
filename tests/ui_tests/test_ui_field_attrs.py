32edf244085c399f231e4c46a49a7a56023b2cd0
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-12-06 14:51:01 +0000

    fixture in field attrs tests file

--- tests/ui_tests/test_ui_field_attrs.py
+++ tests/ui_tests/test_ui_field_attrs.py
@@ -5,20 +5,24 @@ import numpy as np
 from tests.helpers import file  # noqa: F401
 
 
+@pytest.fixture(scope="function")
+def field_attrs_dialog(qtbot):
+
+    dialog = FieldAttrsDialog()
+    qtbot.addWidget(dialog)
+    return dialog
+
+
 @pytest.mark.parametrize("attr_val", ["test", 123, 1.1, np.ushort(12)])
 def test_GIVEN_existing_field_with_attr_WHEN_editing_component_THEN_both_field_and_attrs_are_filled_in_correctly(
-    qtbot, file, attr_val
+    qtbot, file, attr_val, field_attrs_dialog
 ):
     attr_key = "units"
 
     ds = file.create_dataset(name="test", data=123)
     ds.attrs[attr_key] = attr_val
 
-    dialog = FieldAttrsDialog()
+    field_attrs_dialog.fill_existing_attrs(ds)
 
-    qtbot.addWidget(dialog)
-
-    dialog.fill_existing_attrs(ds)
-
-    assert len(dialog.get_attrs()) == 1
-    assert dialog.get_attrs()[attr_key] == attr_val
+    assert len(field_attrs_dialog.get_attrs()) == 1
+    assert field_attrs_dialog.get_attrs()[attr_key] == attr_val
