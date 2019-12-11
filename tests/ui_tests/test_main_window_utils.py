dc2560a5d4a4dc8abd2bfe6fe5955c0292c660fc
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-12-11 12:05:06 +0000

    trigger test

--- tests/ui_tests/test_main_window_utils.py
+++ tests/ui_tests/test_main_window_utils.py
@@ -21,18 +21,39 @@ def tree_view_tab(template):
     return QWidget(template)
 
 
+@pytest.fixture(scope="function")
+def icon_path():
+    return "new_component.png"
+
+
+@pytest.fixture(scope="function")
+def mouse_over_text():
+    return "New Component"
+
+
+@pytest.fixture(scope="function")
+def trigger_method_mock():
+    return Mock()
+
+
 def test_GIVEN_action_properties_WHEN_creating_action_THEN_action_has_expected_attributes(
-    tool_bar, tree_view_tab
+    icon_path, mouse_over_text, trigger_method_mock, tool_bar, tree_view_tab
 ):
-
-    icon_path = "new_component.png"
-    mouse_over_text = "New Component"
-    trigger_method = Mock()
-
     action = create_and_add_toolbar_action(
-        icon_path, mouse_over_text, trigger_method, tool_bar, tree_view_tab
+        icon_path, mouse_over_text, trigger_method_mock, tool_bar, tree_view_tab
     )
 
     assert action.toolTip() == mouse_over_text
     assert action.parent() is tree_view_tab
     assert not action.icon().isNull()
+    assert not action.isEnabled()
+
+
+def test_GIVEN_action_is_triggered_THEN_expected_trigger_method_is_called(
+    icon_path, mouse_over_text, trigger_method_mock, tool_bar, tree_view_tab
+):
+    action = create_and_add_toolbar_action(
+        icon_path, mouse_over_text, trigger_method_mock, tool_bar, tree_view_tab
+    )
+    action.trigger()
+    trigger_method_mock.assert_called_once()
