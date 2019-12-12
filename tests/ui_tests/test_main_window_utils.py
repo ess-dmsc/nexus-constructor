8cc47fc9013d9fa7ad91e354a7ca95d24735594d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-12-12 10:10:09 +0000

    test for selecting transformation list when component already has link

--- tests/ui_tests/test_main_window_utils.py
+++ tests/ui_tests/test_main_window_utils.py
@@ -395,3 +395,44 @@ def test_GIVEN_component_is_selected_WHEN_component_already_has_link_and_changin
     assert not any(
         [action.isEnabled() for action in set_of_all_actions - already_has_link_actions]
     )
+
+
+def test_GIVEN_transformation_list_is_selected_WHEN_component_already_has_link_THEN_create_link_button_is_disabled(
+    component_tree_view,
+    delete_action,
+    duplicate_action,
+    new_rotation_action,
+    new_translation_action,
+    create_link_action,
+    zoom_action,
+    edit_component_action,
+    component_model,
+    set_of_all_actions,
+):
+    # Select the sample in the component tree view
+    sample_component_index = component_tree_view.indexAt(QPoint(0, 0))
+    component_tree_view.setCurrentIndex(sample_component_index)
+    # Add a transformation to the sample
+    component_model.add_link(sample_component_index)
+    # Expand the tree at the sample
+    component_tree_view.expand(sample_component_index)
+    # Retrieve the index of the transformation list and expand the tree at this point
+    transformation_list_index = component_model.index(1, 0, sample_component_index)
+    component_tree_view.setCurrentIndex(transformation_list_index)
+
+    set_button_state(
+        component_tree_view,
+        delete_action,
+        duplicate_action,
+        new_rotation_action,
+        new_translation_action,
+        create_link_action,
+        zoom_action,
+        edit_component_action,
+    )
+
+    already_has_link_actions = {new_translation_action, new_rotation_action}
+    assert all([action.isEnabled() for action in already_has_link_actions])
+    assert not any(
+        [action.isEnabled() for action in set_of_all_actions - already_has_link_actions]
+    )
