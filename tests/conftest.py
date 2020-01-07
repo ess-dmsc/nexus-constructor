43fc969f1212f7e5a4af8904b8f6ee1a86030aa3
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-01-07 13:03:40 +0000

    fixing noqa

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 from tests.chopper_test_helpers import chopper_details  # noqa: F401
-from tests.ui_tests.ui_test_utils import (
+from tests.ui_tests.ui_test_utils import (  # noqa: F401
     template,
     nexus_wrapper,
     instrument,
     add_component_dialog,
     mock_pixel_options,
-)  # noqa: F401
+)
