02ea74c2e57e1c2381431aec5eb24128a21cdf2e
Jack Harper <jack.harper@stfc.ac.uk>
2020-03-02 16:14:02 +0000

    fixing ui tests

--- tests/ui_tests/test_ui_filewriter_command_options.py
+++ tests/ui_tests/test_ui_filewriter_command_options.py
@@ -12,7 +12,7 @@ def test_UI_GIVEN_user_presses_disable_start_time_THEN_start_time_line_edit_is_d
 
 def test_UI_GIVEN_user_presses_disable_stop_time_THEN_stop_time_picker_is_disabled():
     dialog = FilewriterCommandWidget()
-    assert dialog.stop_time_picker.isEnabled()
+    assert not dialog.stop_time_picker.isEnabled()
     dialog.stop_time_enabled.setChecked(False)
     assert dialog.stop_time_picker.isEnabled()
 
@@ -26,6 +26,6 @@ def test_UI_GIVEN_user_presses_enable_start_time_THEN_start_time_picker_is_enabl
 
 def test_UI_GIVEN_user_presses_enable_stop_time_THEN_stop_time_picker_is_enabled():
     dialog = FilewriterCommandWidget()
-    assert dialog.stop_time_picker.isEnabled()
+    assert not dialog.stop_time_picker.isEnabled()
     dialog.stop_time_enabled.setChecked(True)
     assert dialog.stop_time_picker.isEnabled()
