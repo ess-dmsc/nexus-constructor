c07568e4320340010a8b62780f8f3deaaaddc45d
Jack Harper <jack.harper@stfc.ac.uk>
2020-03-09 11:21:06 +0000

    adding ui tests

--- tests/ui_tests/test_ui_filewriter_command_options.py
+++ tests/ui_tests/test_ui_filewriter_command_options.py
@@ -1,3 +1,5 @@
+import uuid
+
 import pytest
 from PySide2.QtCore import QSettings
 from nexus_constructor.filewriter_command_widget import FilewriterCommandWidget
@@ -53,7 +55,7 @@ def settings():
 
 
 def test_UI_settings_are_saved_when_store_settings_is_called(qtbot, settings):
-    nexus_wrapper = NexusWrapper()
+    nexus_wrapper = NexusWrapper(str(uuid.uuid4()))
     instrument = Instrument(nexus_wrapper, {})
     window = FileWriterCtrl(instrument, settings)
     qtbot.addWidget(window)
@@ -91,3 +93,33 @@ def test_UI_settings_are_saved_when_store_settings_is_called(qtbot, settings):
         == use_stop_time
     )
     assert settings.value(FileWriterSettings.FILE_NAME) == filename
+
+
+def test_UI_stored_settings_are_shown_in_window(qtbot, settings):
+    command_broker = "broker:9092/topic2"
+    status_broker = "broker2:9092/topic3"
+    file_broker = "broker3:9092/topic4"
+    use_start_time = True
+    use_stop_time = False
+    filename = "test2.nxs"
+
+    settings.setValue(FileWriterSettings.STATUS_BROKER_ADDR, status_broker)
+    settings.setValue(FileWriterSettings.COMMAND_BROKER_ADDR, command_broker)
+    settings.setValue(FileWriterSettings.FILE_NAME, filename)
+    settings.setValue(FileWriterSettings.USE_START_TIME, use_start_time)
+    settings.setValue(FileWriterSettings.USE_STOP_TIME, use_stop_time)
+    settings.setValue(FileWriterSettings.FILE_BROKER_ADDR, file_broker)
+
+    nexus_wrapper = NexusWrapper(str(uuid.uuid4()))
+    instrument = Instrument(nexus_wrapper, {})
+    window = FileWriterCtrl(instrument, settings)
+    qtbot.addWidget(window)
+
+    window._restore_settings()
+
+    assert window.status_broker_edit.text() == status_broker
+    assert window.command_broker_edit.text() == command_broker
+    assert use_start_time == window.command_widget.start_time_enabled.isChecked()
+    assert use_stop_time == window.command_widget.stop_time_enabled.isChecked()
+    assert filename == window.command_widget.nexus_file_name_edit.text()
+    assert file_broker == window.command_widget.broker_line_edit.text()
