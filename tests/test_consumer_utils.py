d66732fa96d31334eae158e17162401b03506866
Jack Harper <jack.harper@stfc.ac.uk>
2020-03-13 10:17:50 +0000

    adding test for message with no service id

--- tests/test_consumer_utils.py
+++ tests/test_consumer_utils.py
@@ -50,3 +50,38 @@ def test_handle_consumer_message_returns_list_of_files_and_filewriters_with_corr
     assert writer.name == service_id
     assert writer.last_time == last_seen
     assert writer.row == 0
+
+
+def test_handle_consumer_message_works_with_no_service_id_but_returns_no_filewriters():
+    file_name = "test.nxs"
+    job_id = "asdfgh1jk23"
+    start_time = 12345678
+    stop_time = 87654321
+    message = {
+        "file_being_written": file_name,
+        "job_id": job_id,
+        "start_time": start_time,
+        "stop_time": stop_time,
+        "update_interval": 2000,
+    }
+
+    last_seen = 23458789
+    kafka_message = MockMessage(last_seen)
+
+    files = {}
+    writers = {}
+    known_writers, known_files = handle_consumer_message(
+        files, writers, kafka_message, message
+    )
+
+    assert file_name in known_files.keys()
+    file = known_files[file_name]
+
+    assert file.name == file_name
+    assert file.last_time == last_seen
+    assert file.start_time == start_time
+    assert file.stop_time == stop_time
+    assert file.writer_id == "Unknown"
+    assert file.row == 0
+
+    assert len(writers.keys()) == 0
