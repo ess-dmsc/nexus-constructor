66a3ea92fc34a3dae06f51511d74d9d5242a8649
Jack Harper <jack.harper@stfc.ac.uk>
2020-03-31 10:24:23 +0100

    Fixing test names

--- tests/test_model_helpers.py
+++ tests/test_model_helpers.py
@@ -3,7 +3,7 @@ from nexus_constructor.model.helpers import get_item, set_item, remove_item
 import pytest
 
 
-def test_get_item_works_as_expected_when_component_is_in_list():
+def test_get_item_returns_correct_component_when_given_component_is_in_list():
     name = "component1"
     test = Component(name)
 
@@ -11,7 +11,7 @@ def test_get_item_works_as_expected_when_component_is_in_list():
     assert get_item(list_of_components, name) == test
 
 
-def test_get_item_works_as_expected_when_component_is_not_in_list_then_works_when_item_is_set():
+def test_get_item_throws_error_when_component_is_not_in_list_then_works_when_item_is_set():
     name = "component1"
     test = Component(name)
 
