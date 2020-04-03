9a82b003a9e95587bc95d4a05b908ceaec296e76
Jack Harper <jack.harper@stfc.ac.uk>
2020-04-03 14:18:58 +0100

    tests still not working - not sure why

--- nexus_constructor/model/component.py
+++ nexus_constructor/model/component.py
@@ -1,5 +1,7 @@
 import attr
 
+from nexus_constructor.common_attrs import CommonAttrs
+from nexus_constructor.model.attribute import FieldAttribute
 from nexus_constructor.model.group import Group
 
 
@@ -7,8 +9,8 @@ from nexus_constructor.model.group import Group
 class Component(Group):
     @property
     def description(self):
-        return self["description"]
+        return self[CommonAttrs.DESCRIPTION]
 
     @description.setter
     def description(self, new_description: str):
-        self["description"] = new_description
+        self[CommonAttrs.DESCRIPTION] = FieldAttribute(CommonAttrs.DESCRIPTION, new_description)
--- tests/test_model_component.py
+++ tests/test_model_component.py
@@ -1,19 +1,27 @@
+import pytest
 from nexus_constructor.model.component import Component
 
 
 def test_component_set_item_with_brackets_works_with_another_component():
     comp1 = Component("comp1")
-
     comp2 = Component("comp2")
-
     comp1[comp2.name] = comp2
 
     assert comp1[comp2.name] == comp2
 
 
-def test_component_set_item_with_brackets_works_with_string():
+
+def test_component_set_item_with_brackets_throws_when_string_is_entered():
     comp1 = Component("comp1")
     some_field_value = "test"
-    # TODO: this should fail when setting before getting
-    comp1["some_field"] = some_field_value
-    assert comp1["some_field"] == some_field_value
+    with pytest.raises(AttributeError):
+        comp1["some_field"] = some_field_value
+        assert comp1["some_field"] == some_field_value
+
+
+def test_component_set_description_correctly_sets_description():
+    comp = Component("comp3")
+    description = "a component"
+    comp.description = description
+
+    assert comp.description.values == description
