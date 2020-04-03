21c22e1909a6326377613d445e16677f939754f6
Jack Harper <jack.harper@stfc.ac.uk>
2020-04-03 14:52:51 +0100

    Fixing transformation attribute getting and setting

--- nexus_constructor/model/transformation.py
+++ nexus_constructor/model/transformation.py
@@ -3,6 +3,7 @@ from PySide2.Qt3DCore import Qt3DCore
 from PySide2.QtGui import QVector3D, QMatrix4x4
 import numpy as np
 
+from nexus_constructor.model.attribute import FieldAttribute
 from nexus_constructor.model.group import Group
 from nexus_constructor.model.helpers import get_item, set_item
 from nexus_constructor.common_attrs import CommonAttrs
@@ -22,15 +23,15 @@ class Transformation:
 
     @property
     def type(self):
-        return get_item(self.attributes, CommonAttrs.TRANSFORMATION_TYPE)
+        return get_item(self.attributes, CommonAttrs.TRANSFORMATION_TYPE).values
 
     @type.setter
     def type(self, new_type):
-        self.attributes[CommonAttrs.TRANSFORMATION_TYPE] = new_type
+        set_item(self.attributes, CommonAttrs.TRANSFORMATION_TYPE, FieldAttribute(CommonAttrs.TRANSFORMATION_TYPE, new_type))
 
     @property
     def vector(self):
-        vector = get_item(self.attributes, CommonAttrs.VECTOR)
+        vector = get_item(self.attributes, CommonAttrs.VECTOR).values
         return (
             QVector3D(vector[0], vector[1], vector[2]) if vector is not None else None
         )
@@ -38,13 +39,17 @@ class Transformation:
     @vector.setter
     def vector(self, new_vector: QVector3D):
         vector_as_np_array = np.array([new_vector.x(), new_vector.y(), new_vector.z()])
-        set_item(self.attributes, CommonAttrs.VECTOR, vector_as_np_array)
+        set_item(self.attributes, CommonAttrs.VECTOR, FieldAttribute(CommonAttrs.VECTOR, vector_as_np_array))
 
     @property
     def ui_value(self):
-        # TODO: get proper ui value
-        get_item(self.attributes, CommonAttrs.UI_VALUE)
-        return 0
+        if isinstance(self.value, (float, int)):
+            return float(self.value)
+        return float(get_item(self.attributes, CommonAttrs.UI_VALUE).values)
+
+    @ui_value.setter
+    def ui_value(self, new_value: float):
+        set_item(self.attributes, CommonAttrs.UI_VALUE, FieldAttribute(CommonAttrs.UI_VALUE, new_value))
 
     @property
     def qmatrix(self) -> QMatrix4x4:
@@ -65,8 +70,8 @@ class Transformation:
 
     @property
     def units(self):
-        return get_item(self.attributes, CommonAttrs.UNITS)
+        return get_item(self.attributes, CommonAttrs.UNITS).values
 
     @units.setter
     def units(self, new_units):
-        set_item(self.attributes, CommonAttrs.UNITS, new_units)
+        set_item(self.attributes, CommonAttrs.UNITS, FieldAttribute(CommonAttrs.UNITS, new_units))
