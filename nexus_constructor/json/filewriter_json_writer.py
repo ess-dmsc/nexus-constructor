c780300fde0f4e0f3f35159d30b5e8c4390c097c
Jack Harper <jack.harper@stfc.ac.uk>
2020-05-21 15:24:06 +0100

    type hints, removing unused constant

--- nexus_constructor/json/filewriter_json_writer.py
+++ nexus_constructor/json/filewriter_json_writer.py
@@ -1,11 +1,12 @@
-from typing import Union
+from io import FileIO
+from typing import Union, Any
 import h5py
 from nexus_constructor.common_attrs import CommonAttrs
 
 NexusObject = Union[h5py.Group, h5py.Dataset, h5py.SoftLink]
 
 
-def write_nexus_structure_to_json(data, file):
+def write_nexus_structure_to_json(data: Any, file: FileIO):
     """
     Generates and writes the nexus structure to a given JSON file object.
     The json description can be used by the file writer (github.com/ess-dmsc/kafka-to-nexus) to create a NeXus file
@@ -28,4 +29,3 @@ def cast_to_int(data):
 
 
 ATTR_NAME_BLACKLIST = [CommonAttrs.DEPENDEE_OF, CommonAttrs.UI_VALUE]
-NX_CLASS_BLACKLIST = ["NXgroup", CommonAttrs.NC_STREAM]
