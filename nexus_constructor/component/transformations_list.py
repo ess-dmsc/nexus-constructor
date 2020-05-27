67903fe49098d553dab1b4d19e1019acc8adc1d8
Jack Harper <jack.harper@stfc.ac.uk>
2020-05-27 16:02:58 +0100

    adding old methods back to transformations_list.py

--- nexus_constructor/component/transformations_list.py
+++ nexus_constructor/component/transformations_list.py
@@ -1,3 +1,5 @@
+from nexus_constructor.model.transformation import Transformation
+
 TRANSFORM_STR = "/transformations/"
 LINK_STR = "has_link"
 
@@ -16,9 +18,37 @@ class TransformationsList(list):
 
         self.link = LinkTransformation(self)
 
+    def _has_direct_link(self) -> bool:
+        return (
+            len(self) == 0
+            and self.parent_component.depends_on is not None
+            and TRANSFORM_STR in self.parent_component.depends_on.absolute_path
+        )
+
+    def _transform_has_external_link(self, transformation: Transformation) -> bool:
+        raise NotImplementedError
+        if transformation.depends_on is None:
+            return False
+        # return (
+        #     TRANSFORM_STR in transformation.depends_on.absolute_path
+        #     and (self.parent_component.absolute_path + TRANSFORM_STR)
+        #     not in transformation.depends_on.absolute_path
+        # )
+
+    def _has_indirect_link(self) -> bool:
+        for transform in self:
+            if self._transform_has_external_link(transform):
+                return True
+        return False
+
     @property
     def has_link(self) -> bool:
         try:
             return self[-1].depends_on is not None
         except IndexError:
             return False
+
+    @has_link.setter
+    def has_link(self, value: bool):
+        self.parent_component.set_attribute_value(LINK_STR, value
+        )
