183edf01e28b4777e0e0c388ddee38fd409bbdfe
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-05-28 12:26:32 +0100

    Removing signal in fieldwidget

--- nexus_constructor/field_widget.py
+++ nexus_constructor/field_widget.py
@@ -67,7 +67,6 @@ class FieldNameLineEdit(QLineEdit):
 class FieldWidget(QFrame):
     # Used for deletion of field
     something_clicked = Signal()
-    value_changed = Signal(float)
 
     def dataset_type_changed(self, _):
         self.value_line_edit.validator().dataset_type_combo = self.value_type_combo
@@ -116,7 +115,6 @@ class FieldWidget(QFrame):
 
         self.value_line_edit = QLineEdit()
         self.value_line_edit.setPlaceholderText("value")
-        self.value_line_edit.textChanged.connect(self._update_ui_value)
 
         self._set_up_value_validator(False)
         self.dataset_type_changed(0)
@@ -166,13 +164,6 @@ class FieldWidget(QFrame):
 
         # Set the layout for the default field type
         self.field_type_changed()
-
-    def _update_ui_value(self, text: str):
-        try:
-            ui_value = float(text)
-            self.value_changed.emit(ui_value)
-        except ValueError:
-            return
 
     def _set_up_name_validator(self):
         field_widgets = []
