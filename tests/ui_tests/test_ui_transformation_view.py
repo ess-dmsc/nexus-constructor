d62ef327dbb6da1031aaaac4fe5ecfceb61720e6
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-05-28 17:49:33 +0100

    Most asserts passing in first test

--- tests/ui_tests/test_ui_transformation_view.py
+++ tests/ui_tests/test_ui_transformation_view.py
@@ -2,28 +2,47 @@ import h5py
 from PySide2.QtGui import QVector3D
 from mock import Mock
 
+from nexus_constructor.model.component import Component
+from nexus_constructor.model.dataset import Dataset, DatasetMetadata
 from nexus_constructor.model.entry import Instrument
+from nexus_constructor.model.transformation import Transformation
 from nexus_constructor.transformation_view import EditRotation, EditTranslation
 from nexus_constructor.validators import FieldType
 import numpy as np
 from pytestqt.qtbot import QtBot  # noqa: F401
 import pytest
 
-pytest.skip("Disabled whilst working on model change", allow_module_level=True)
+
+def create_transformation(vector):
+    transformation = Transformation("Transformation1", dataset=None, values=[])
+    transformation.vector = vector
+    return transformation
+
+
+@pytest.fixture
+def instrument():
+    return Instrument()
+
+
+@pytest.fixture
+def component():
+    return Component("Component", [])
+
+
+@pytest.fixture
+def values():
+    return Dataset(name="", dataset=DatasetMetadata(type="Double", size=[1]), values="")
 
 
 def test_UI_GIVEN_scalar_vector_WHEN_creating_translation_view_THEN_ui_is_filled_correctly(
-    qtbot, nexus_wrapper
+    qtbot, instrument, component, values
 ):
-
-    instrument = Instrument(nexus_wrapper, {})
-
-    component = instrument.create_component("test", "NXaperture", "")
 
     x = 1
     y = 0
     z = 0
-    transform = component.add_translation(QVector3D(x, y, z), name="test")
+    transform = component.add_translation(QVector3D(x, y, z), name="transform")
+    transform.values = values
 
     view = EditTranslation(parent=None, transformation=transform, instrument=instrument)
     qtbot.addWidget(view)
@@ -31,14 +50,15 @@ def test_UI_GIVEN_scalar_vector_WHEN_creating_translation_view_THEN_ui_is_filled
     assert view.transformation_frame.x_spinbox.value() == x
     assert view.transformation_frame.y_spinbox.value() == y
     assert view.transformation_frame.z_spinbox.value() == z
-    assert view.transformation_frame.value_spinbox.value() == 1
-    assert view.transformation_frame.magnitude_widget.value[()] == 1
+    # assert view.transformation_frame.value_spinbox.value() == 1
+    assert view.transformation_frame.magnitude_widget.value.values == ""
     assert (
         view.transformation_frame.magnitude_widget.field_type
         == FieldType.scalar_dataset
     )
 
 
+@pytest.mark.skip
 def test_UI_GIVEN_scalar_angle_WHEN_creating_rotation_view_THEN_ui_is_filled_correctly(
     qtbot, nexus_wrapper
 ):
@@ -68,6 +88,7 @@ def test_UI_GIVEN_scalar_angle_WHEN_creating_rotation_view_THEN_ui_is_filled_cor
     )
 
 
+@pytest.mark.skip
 def test_UI_GIVEN_array_dataset_as_magnitude_WHEN_creating_translation_THEN_ui_is_filled_correctly(
     qtbot, file, nexus_wrapper
 ):
@@ -96,6 +117,7 @@ def test_UI_GIVEN_array_dataset_as_magnitude_WHEN_creating_translation_THEN_ui_i
     )
 
 
+@pytest.mark.skip
 def test_UI_GIVEN_stream_group_as_angle_WHEN_creating_rotation_THEN_ui_is_filled_correctly(
     qtbot, file, nexus_wrapper
 ):
@@ -138,6 +160,7 @@ def test_UI_GIVEN_stream_group_as_angle_WHEN_creating_rotation_THEN_ui_is_filled
     assert view.transformation_frame.magnitude_widget.value["source"][()] == source
 
 
+@pytest.mark.skip
 def test_UI_GIVEN_link_as_rotation_magnitude_WHEN_creating_rotation_view_THEN_ui_is_filled_correctly(
     qtbot, nexus_wrapper
 ):
@@ -166,6 +189,7 @@ def test_UI_GIVEN_link_as_rotation_magnitude_WHEN_creating_rotation_view_THEN_ui
     assert view.transformation_frame.magnitude_widget.value.path == path
 
 
+@pytest.mark.skip
 def test_UI_GIVEN_vector_updated_WHEN_saving_view_changes_THEN_model_is_updated(
     qtbot, nexus_wrapper
 ):
@@ -196,6 +220,7 @@ def test_UI_GIVEN_vector_updated_WHEN_saving_view_changes_THEN_model_is_updated(
     assert transform.vector == QVector3D(new_x, new_y, new_z)
 
 
+@pytest.mark.skip
 def test_UI_GIVEN_view_gains_focus_WHEN_transformation_view_exists_THEN_spinboxes_are_enabled(
     qtbot, nexus_wrapper
 ):
@@ -221,6 +246,7 @@ def test_UI_GIVEN_view_gains_focus_WHEN_transformation_view_exists_THEN_spinboxe
     assert view.transformation_frame.name_line_edit.isEnabled()
 
 
+@pytest.mark.skip
 def test_UI_GIVEN_view_loses_focus_WHEN_transformation_view_exists_THEN_spinboxes_are_disabled(
     qtbot, nexus_wrapper
 ):
@@ -246,6 +272,7 @@ def test_UI_GIVEN_view_loses_focus_WHEN_transformation_view_exists_THEN_spinboxe
     assert not view.transformation_frame.name_line_edit.isEnabled()
 
 
+@pytest.mark.skip
 def test_UI_GIVEN_new_values_are_provided_WHEN_save_changes_is_called_THEN_transformation_changed_signal_is_called_to_update_3d_view(
     qtbot, nexus_wrapper
 ):
