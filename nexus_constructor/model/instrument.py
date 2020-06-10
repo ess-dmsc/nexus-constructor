8e45e676c8095e9887a17e197ec596a55f313870
Matthew D Jones <matthew.d.jones@tessella.com>
2020-06-10 13:51:58 +0100

    Update and test as_dict method for instrument model

--- nexus_constructor/model/instrument.py
+++ nexus_constructor/model/instrument.py
@@ -1,13 +1,17 @@
 from nexus_constructor.model.component import Component
 from nexus_constructor.model.group import Group
+from typing import Dict, Any
+
+SAMPLE_NAME = "sample"
+INSTRUMENT_NAME = "instrument"
 
 
 class Instrument(Group):
     def __init__(self):
-        super().__init__("instrument")
+        super().__init__(INSTRUMENT_NAME)
         self.nx_class = "NXinstrument"
 
-        self.sample = Component("sample")
+        self.sample = Component(SAMPLE_NAME)
         self.sample.nx_class = "NXsample"
         self.component_list = [self.sample]
 
@@ -16,3 +20,15 @@ class Instrument(Group):
 
     def remove_component(self, component: Component):
         self.component_list.remove(component)
+
+    def as_dict(self) -> Dict[str, Any]:
+        dictionary = super(Instrument, self).as_dict()
+        # Put components (other than sample) in children
+        dictionary["children"].extend(
+            [
+                component.as_dict()
+                for component in self.component_list
+                if component.name != SAMPLE_NAME
+            ]
+        )
+        return dictionary
--- tests/model/test_entry.py
+++ tests/model/test_entry.py
@@ -1,5 +1,5 @@
 from nexus_constructor.model.entry import Entry
-from nexus_constructor.model.instrument import Instrument
+from nexus_constructor.model.instrument import Instrument, SAMPLE_NAME, INSTRUMENT_NAME
 
 
 def test_entry_as_dict_contains_sample_and_instrument():
@@ -8,5 +8,5 @@ def test_entry_as_dict_contains_sample_and_instrument():
     dictionary_output = test_entry.as_dict()
 
     child_names = [child["name"] for child in dictionary_output["children"]]
-    assert "sample" in child_names
-    assert "instrument" in child_names
+    assert SAMPLE_NAME in child_names
+    assert INSTRUMENT_NAME in child_names
