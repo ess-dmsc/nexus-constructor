c1103aba7cfe42f729a4f9c74fdd3577213a3459
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-06-16 13:05:30 +0100

    restarting

--- nexus_constructor/model/model.py
+++ nexus_constructor/model/model.py
@@ -6,6 +6,8 @@ from typing import Dict, Any
 from nexus_constructor.model.entry import Entry
 from nexus_constructor.model.instrument import Instrument
 
+ignore = ["entry", "instrument"]
+
 
 class Signals(QObject):
     """
@@ -44,15 +46,4 @@ class Model:
             return False
 
     def parse_json(self, json_entry: dict):
-
-        name = json_entry.get("name")
-
-        if name == "nexus_structure":
-            self.parse_json(json_entry.get("children"))
-        elif name == "entry":
-            print(json_entry)
-        elif name == "instrument":
-            print(json_entry)
-        elif name == "transformations":
-            for child in json_entry.get("children"):
-                self.parse_json(child)
+        pass
