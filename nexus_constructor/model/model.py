0714b2e5f9cadb25995f3a66ced91cc12d3cc95a
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-06-16 09:25:56 +0100

    getting name from json

--- nexus_constructor/model/model.py
+++ nexus_constructor/model/model.py
@@ -1,5 +1,8 @@
+import json
+
 from PySide2.QtCore import QObject, Signal
 from typing import Dict, Any
+
 from nexus_constructor.model.entry import Entry
 from nexus_constructor.model.instrument import Instrument
 
@@ -17,18 +20,39 @@ class Signals(QObject):
     show_entries_dialog = Signal("QVariant", "QVariant")
 
 
-def as_instrument(dct):
-    if dct["name"] == "instrument":
-        return Instrument()
-
-
 class Model:
     def __init__(self, entry: Entry):
         self.signals = Signals()
         self.entry = entry
+        self.temp_entry = None
 
     def as_dict(self) -> Dict[str, Any]:
         return {"nexus_structure": {"children": [self.entry.as_dict()]}}
 
-    def load_json_file(self, json_file: dict):
-        return False
+    def load_json_file(self, json_data: str):
+
+        self.temp_entry = Entry()
+        self.entry.instrument = Instrument()
+
+        entry = json.loads(json_data, object_hook=self.parse_json)
+
+        if entry is not None:
+            self.entry = self.temp_entry
+            self.temp_entry = None
+            return True
+        else:
+            return False
+
+    def parse_json(self, json_entry: dict):
+
+        name = json_entry.get("name")
+
+        if name == "nexus_structure":
+            self.parse_json(json_entry.get("children"))
+        elif name == "entry":
+            print(json_entry)
+        elif name == "instrument":
+            print(json_entry)
+        elif name == "transformations":
+            for child in json_entry.get("children"):
+                self.parse_json(child)
