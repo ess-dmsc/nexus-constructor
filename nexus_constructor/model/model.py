21a3086568c7f78b34f64f97d44e6fdf8c3d4e47
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-06-16 16:40:18 +0100

    not using object hook

--- nexus_constructor/model/model.py
+++ nexus_constructor/model/model.py
@@ -3,10 +3,20 @@ import json
 from PySide2.QtCore import QObject, Signal
 from typing import Dict, Any
 
+from nexus_constructor.model.component import Component
 from nexus_constructor.model.entry import Entry
 from nexus_constructor.model.instrument import Instrument
 
-ignore = ["entry", "instrument"]
+ignore = ["entry", "instrument", "transformations", "NX_class"]
+
+
+def _parse_nx_class(entry: list):
+
+    for item in entry:
+        if item.get("NX_class"):
+            return item.get("values")
+
+    return None
 
 
 class Signals(QObject):
@@ -34,11 +44,16 @@ class Model:
     def load_json_file(self, json_data: str):
 
         self.temp_entry = Entry()
-        self.entry.instrument = Instrument()
+        self.temp_entry.instrument = Instrument()
 
-        entry = json.loads(json_data, object_hook=self.parse_json)
+        json_dict = json.loads(json_data)
 
-        if entry is not None:
+        try:
+            children_list = json_dict["nexus_structure"]["children"][0]["children"]
+        except KeyError:
+            return False
+
+        if all(self.parse_json(child) for child in children_list):
             self.entry = self.temp_entry
             self.temp_entry = None
             return True
@@ -46,4 +61,28 @@ class Model:
             return False
 
     def parse_json(self, json_entry: dict):
-        pass
+
+        name = json_entry.get("name")
+
+        if name == "instrument":
+            return True
+
+        elif name and name not in ignore:
+
+            nx_class = _parse_nx_class(json_entry.get("attributes"))
+
+            if nx_class is None:
+                return False
+
+            elif nx_class == "NX_sample":
+                component = self.temp_entry.instrument.sample
+                component.name = name
+            else:
+                component = Component(name)
+                self.temp_entry.instrument.add_component(component)
+
+            # todo: recover transformations
+
+            return True
+
+        return False
