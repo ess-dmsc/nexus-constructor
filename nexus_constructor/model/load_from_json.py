806fc32d4e7432657419576df7f23b273b4517ed
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-06-17 14:27:13 +0100

    tidying

--- nexus_constructor/model/load_from_json.py
+++ nexus_constructor/model/load_from_json.py
@@ -1,6 +1,8 @@
 import json
 import logging
 
+from PySide2.QtWidgets import QWidget
+
 from nexus_constructor.model.component import Component
 from nexus_constructor.model.entry import Entry
 from nexus_constructor.model.instrument import Instrument
@@ -9,17 +11,15 @@ from nexus_constructor.ui_utils import show_warning_dialog
 IGNORE = ["entry", "instrument", "transformations", "NX_class"]
 
 
-def _read_nx_class(entry: list):
-
+def _read_nx_class(entry: list) -> str:
     for item in entry:
         if item.get("name") == "NX_class":
             return item.get("values")
 
-    return None
+    return ""
 
 
 def _read_transformations(entry: list):
-
     for item in entry:
         if item.get("name") == "transformations":
             return item.get("children")
@@ -27,14 +27,21 @@ def _read_transformations(entry: list):
     return None
 
 
+def _retrieve_children_list(json_dict: dict) -> list:
+    try:
+        return json_dict["nexus_structure"]["children"][0]["children"]
+    except (KeyError, IndexError):
+        return []
+
+
 class JSONReader:
-    def __init__(self, parent):
+    def __init__(self, parent: QWidget):
 
         self.entry = Entry()
         self.entry.instrument = Instrument()
         self.parent = parent
 
-    def load_model_from_json(self, filename: str):
+    def load_model_from_json(self, filename: str) -> bool:
 
         with open(filename, "r") as json_file:
 
@@ -51,23 +58,24 @@ class JSONReader:
                 )
                 return False
 
-            try:
-                children_list = json_dict["nexus_structure"]["children"][0]["children"]
-            except KeyError:
+            children_list = _retrieve_children_list(json_dict)
+
+            if not children_list:
+                logging.info("Unable to locate components in JSON file.")
                 return False
 
             return all(self._read_json_object(child) for child in children_list)
 
-    def _read_json_object(self, json_entry: dict):
+    def _read_json_object(self, json_object: dict) -> bool:
 
-        name = json_entry.get("name")
+        name = json_object.get("name")
 
         if name == "instrument":
             return True
 
         elif name and name not in IGNORE:
 
-            nx_class = _read_nx_class(json_entry.get("attributes"))
+            nx_class = _read_nx_class(json_object.get("attributes"))
 
             if nx_class is None:
                 logging.warning("Unable to determine NXclass.")
@@ -80,15 +88,15 @@ class JSONReader:
                 component = Component(name)
                 self.entry.instrument.add_component(component)
 
-            transformations = _read_transformations(json_entry.get("children"))
+            transformations = _read_transformations(json_object.get("children"))
 
-            if transformations is None:
-                logging.warning("Unable to find transformations entry for component.")
+            if not transformations:
+                logging.info("Unable to find transformations entry for component.")
                 return False
-            else:
-                for transformation in transformations:
-                    # todo: transformation reading
-                    pass
+
+            for transformation in transformations:
+                # todo: transformation reading
+                pass
 
             return True
 
--- tests/ui_tests/test_load_from_json.py
+++ tests/ui_tests/test_load_from_json.py
@@ -61,4 +61,58 @@ def test_GIVEN_json_with_missing_value_WHEN_loading_from_json_THEN_json_loader_r
     """
 
     with patch('nexus_constructor.model.load_from_json.open', mock_open(read_data=json_string), create=True):
-        assert not json_reader.load_model_from_json("filename")
+        assert not json_reader.load_model_from_json("")
+
+def test_GIVEN_unable_to_find_children_field_WHEN_loading_from_json_THEN_json_loader_returns_false(json_reader):
+
+    json_string = """
+    {
+      "nexus_structure": {
+        "children": [
+          {
+            "name": "entry",
+            "type": "group",
+            "attributes": [
+              {
+                "name":  "NX_class",
+                "values":  "NXentry"
+              }
+            ],
+            "crumpet": [
+              {
+                "name": "instrument",
+                "type": "group",
+                "attributes": [
+                  {
+                    "name":  "NX_class",
+                    "values":  "NXinstrument"
+                  }
+                ],
+                "children": []
+              },
+              {
+                "name": "sample",
+                "type": "group",
+                "attributes": [
+                  {
+                    "name":  "NX_class",
+                    "values":  "NXsample"
+                  }
+                ],
+                "children": [
+                  {
+                    "type": "group",
+                    "name": "transformations",
+                    "children": []
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    }
+    """
+
+    with patch('nexus_constructor.model.load_from_json.open', mock_open(read_data=json_string), create=True):
+        assert not json_reader.load_model_from_json("")
