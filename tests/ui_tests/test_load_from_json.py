b1a762006896413ee2193448f3715dc8a94b6e38
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-06-17 18:28:32 +0100

    parametrize for nx class test

--- tests/ui_tests/test_load_from_json.py
+++ tests/ui_tests/test_load_from_json.py
@@ -3,16 +3,20 @@ import json
 import pytest
 from mock import patch, mock_open
 
-from nexus_constructor.model.load_from_json import JSONReader, _retrieve_children_list
+from nexus_constructor.model.load_from_json import (
+    JSONReader,
+    _retrieve_children_list,
+    _read_nx_class,
+)
 
 
 @pytest.fixture(scope="function")
-def json_reader(template):
+def json_reader(template) -> JSONReader:
     return JSONReader(template)
 
 
 @pytest.fixture(scope="function")
-def nexus_json_dictionary():
+def nexus_json_dictionary() -> dict:
     json_string = """
     {
       "nexus_structure": {
@@ -62,7 +66,9 @@ def nexus_json_dictionary():
     return json.loads(json_string)
 
 
-def test_GIVEN_json_with_missing_value_WHEN_loading_from_json_THEN_json_loader_returns_false(json_reader):
+def test_GIVEN_json_with_missing_value_WHEN_loading_from_json_THEN_json_loader_returns_false(
+    json_reader,
+):
 
     json_string = """
     {
@@ -111,222 +117,40 @@ def test_GIVEN_json_with_missing_value_WHEN_loading_from_json_THEN_json_loader_r
     }
     """
 
-    with patch('nexus_constructor.model.load_from_json.open', mock_open(read_data=json_string), create=True):
+    with patch(
+        "nexus_constructor.model.load_from_json.open",
+        mock_open(read_data=json_string),
+        create=True,
+    ):
         assert not json_reader.load_model_from_json("")
 
 
-def test_GIVEN_unable_to_find_nexus_structure_field_WHEN_loading_from_json_THEN_json_loader_returns_false(json_reader):
+def test_GIVEN_unable_to_find_nexus_structure_field_WHEN_loading_from_json_THEN_json_loader_returns_false(
+    nexus_json_dictionary,
+):
 
-    json_string = """
-    {
-      "crumpet": {
-        "children": [
-          {
-            "name": "entry",
-            "type": "group",
-            "attributes": [
-              {
-                "name":  "NX_class",
-                "values":  "NXentry"
-              }
-            ],
-            "children": [
-              {
-                "name": "instrument",
-                "type": "group",
-                "attributes":
-                  {
-                    "NX_class":  "NXinstrument"
-                  },
-                "children": []
-              },
-              {
-                "name": "sample",
-                "type": "group",
-                "attributes": [
-                  {
-                    "name":  "NX_class",
-                    "values":  "NXsample"
-                  }
-                ],
-                "children": [
-                  {
-                    "type": "group",
-                    "name": "transformations",
-                    "children": []
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    }
-    """
-
-    with patch('nexus_constructor.model.load_from_json.open', mock_open(read_data=json_string), create=True):
-        assert not json_reader.load_model_from_json("")
-
-    assert not _retrieve_children_list(json.loads(json_string))
+    del nexus_json_dictionary["nexus_structure"]
+    assert not _retrieve_children_list(nexus_json_dictionary)
 
 
-def test_GIVEN_unable_to_find_first_children_field_WHEN_loading_from_json_THEN_json_loader_returns_false(json_reader):
+def test_GIVEN_unable_to_find_first_children_field_WHEN_loading_from_json_THEN_json_loader_returns_false(
+    nexus_json_dictionary,
+):
 
-    json_string = """
-    {
-      "nexus_structure": {
-        "crumpet": [
-          {
-            "name": "entry",
-            "type": "group",
-            "attributes": [
-              {
-                "name":  "NX_class",
-                "values":  "NXentry"
-              }
-            ],
-            "children": [
-              {
-                "name": "instrument",
-                "type": "group",
-                "attributes":
-                  {
-                    "NX_class":  "NXinstrument"
-                  },
-                "children": []
-              },
-              {
-                "name": "sample",
-                "type": "group",
-                "attributes": [
-                  {
-                    "name":  "NX_class",
-                    "values":  "NXsample"
-                  }
-                ],
-                "children": [
-                  {
-                    "type": "group",
-                    "name": "transformations",
-                    "children": []
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    }
-    """
-
-    with patch('nexus_constructor.model.load_from_json.open', mock_open(read_data=json_string), create=True):
-        assert not json_reader.load_model_from_json("")
-
-    assert not _retrieve_children_list(json.loads(json_string))
+    del nexus_json_dictionary["nexus_structure"]["children"]
+    assert not _retrieve_children_list(nexus_json_dictionary)
 
 
-def test_GIVEN_unable_to_find_second_children_field_WHEN_loading_from_json_THEN_json_loader_returns_false(json_reader):
+def test_GIVEN_unable_to_find_second_children_field_WHEN_loading_from_json_THEN_json_loader_returns_false(
+    nexus_json_dictionary,
+):
 
-    json_string = """
-    {
-      "nexus_structure": {
-        "children": [
-          {
-            "name": "entry",
-            "type": "group",
-            "attributes": [
-              {
-                "name":  "NX_class",
-                "values":  "NXentry"
-              }
-            ],
-            "crumpet": [
-              {
-                "name": "instrument",
-                "type": "group",
-                "attributes":
-                  {
-                    "NX_class":  "NXinstrument"
-                  },
-                "children": []
-              },
-              {
-                "name": "sample",
-                "type": "group",
-                "attributes": [
-                  {
-                    "name":  "NX_class",
-                    "values":  "NXsample"
-                  }
-                ],
-                "children": [
-                  {
-                    "type": "group",
-                    "name": "transformations",
-                    "children": []
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    }
-    """
-
-    with patch('nexus_constructor.model.load_from_json.open', mock_open(read_data=json_string), create=True):
-        assert not json_reader.load_model_from_json("")
-
-    assert not _retrieve_children_list(json.loads(json_string))
+    del nexus_json_dictionary["nexus_structure"]["children"][0]["children"]
+    assert not _retrieve_children_list(nexus_json_dictionary)
 
 
-def test_GIVEN_no_nx_class_for_component_WHEN_loading_from_json_THEN_json_loader_returns_false(json_reader):
-
-    json_string = """
-    {
-      "nexus_structure": {
-        "children": [
-          {
-            "name": "entry",
-            "type": "group",
-            "attributes": [
-              {
-                "name":  "NX_class",
-                "values":  "NXentry"
-              }
-            ],
-            "children": [
-              {
-                "name": "instrument",
-                "type": "group",
-                "attributes":
-                  {
-                    "NX_class":  "NXinstrument"
-                  },
-                "children": []
-              },
-              {
-                "name": "sample",
-                "type": "group",
-                "attributes": [
-                  {
-                    "name":  "NX_class"
-                  }
-                ],
-                "children": [
-                  {
-                    "type": "group",
-                    "name": "transformations",
-                    "children": []
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    }
-    """
-
-    with patch('nexus_constructor.model.load_from_json.open', mock_open(read_data=json_string), create=True):
-        assert not json_reader.load_model_from_json("")
+@pytest.mark.parametrize("class_attribute", [[{"name": "NX_class"}], [{"name": "123"}]])
+def test_GIVEN_no_nx_class_values_for_component_WHEN_loading_from_json_THEN_json_loader_returns_false(
+    class_attribute,
+):
+    assert not _read_nx_class(class_attribute)
