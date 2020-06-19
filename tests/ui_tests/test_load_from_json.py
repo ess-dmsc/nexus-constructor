4e8c8ed53dd7d9fd1bb6a18b2f0a3a78779ffd3a
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-06-19 10:09:13 +0100

    fixing typo

--- tests/ui_tests/test_load_from_json.py
+++ tests/ui_tests/test_load_from_json.py
@@ -161,7 +161,7 @@ def test_GIVEN_no_nx_class_values_for_component_WHEN_loading_from_json_THEN_json
     "class_attribute",
     [[{"name": "NX_class", "values": "NXmonitor"}], [{"NX_class": "NXmonitor"}]],
 )
-def test_GIVEN_nx_class_in_different_formations_WHEN_reading_class_information_THEN_read_nx_class_recognises_both_formats(
+def test_GIVEN_nx_class_in_different_formats_WHEN_reading_class_information_THEN_read_nx_class_recognises_both_formats(
     class_attribute,
 ):
 
