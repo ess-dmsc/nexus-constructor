2674a4afc845372d013ea4800f4f4c3d773ac16d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-06-24 15:27:19 +0100

    full spectrum of get transformation property tests

--- tests/model/test_transformation_reader.py
+++ tests/model/test_transformation_reader.py
@@ -1,8 +1,13 @@
 import json
 
 import pytest
+from mock import Mock
 
-from nexus_constructor.model.load_from_json import _contains_transformations
+from nexus_constructor.model.component import Component
+from nexus_constructor.model.load_from_json import (
+    _contains_transformations,
+    TransformationReader,
+)
 
 
 @pytest.fixture(scope="function")
@@ -10,70 +15,48 @@ def transformation_json():
     json_string = """
     {
       "type":"group",
-      "name":"source",
+      "name":"transformations",
       "children":[
         {
           "type":"dataset",
-          "name":"depends_on",
+          "name":"location",
           "dataset":{
-            "type":"string"
+            "type":"double"
           },
-          "values":"/entry/instrument/source/transformations/location"
-        },
-        {
-          "type":"dataset",
-          "name":"name",
-          "dataset":{
-            "type":"string"
-          },
-          "values":"source"
-        },
-        {
-          "type":"group",
-          "name":"transformations",
-          "children":[
-            {
-              "type":"dataset",
-              "name":"location",
-              "dataset":{
-                "type":"double"
-              },
-              "values":0.0,
-              "attributes":[
-                {
-                  "name":"units",
-                  "values":"m"
-                },
-                {
-                  "name":"transformation_type",
-                  "values":"translation"
-                },
-                {
-                  "name":"vector",
-                  "values":[
-                    0.0,
-                    0.0,
-                    0.0
-                  ],
-                  "type":"double"
-                },
-                {
-                  "name":"depends_on",
-                  "values":"."
-                },
-                {
-                  "name":"NX_class",
-                  "values":"NXtransformation"
-                }
-              ]
-            }
-          ],
+          "values":0.0,
           "attributes":[
             {
+              "name":"units",
+              "values":"m"
+            },
+            {
+              "name":"transformation_type",
+              "values":"translation"
+            },
+            {
+              "name":"vector",
+              "values":[
+                0.0,
+                0.0,
+                0.0
+              ],
+              "type":"double"
+            },
+            {
+              "name":"depends_on",
+              "values":"."
+            },
+            {
               "name":"NX_class",
-              "values":"NXtransformations"
+              "values":"NXtransformation"
             }
           ]
+        }
+      ],
+      "attributes":[
+        {
+          "name":"NX_class",
+          "values":"NXtransformations"
         }
       ]
     }
@@ -83,33 +66,69 @@ def transformation_json():
 
 @pytest.fixture(scope="function")
 def transformation_reader(transformation_json):
-    pass
+    parent_component = Mock(spec=Component)
+    parent_component.name = "ParentComponentName"
+    entry = [transformation_json]
+    return TransformationReader(parent_component, entry)
 
 
 @pytest.mark.parametrize("class_value", ["NXtransformation", "NXtransformations"])
 def test_GIVEN_transformation_in_attributes_WHEN_checking_for_transformation_THEN_contains_transformations_returns_true(
     class_value, transformation_json
 ):
-    transformations = transformation_json["children"][2]
-    transformations["attributes"][0]["values"] = class_value
-    assert _contains_transformations(transformations)
+    transformation_json["attributes"][0]["values"] = class_value
+    assert _contains_transformations(transformation_json)
 
 
 def test_GIVEN_no_transformation_class_in_attributes_WHEN_checking_for_transformations_THEN_contains_transformations_returns_false(
     transformation_json,
 ):
-    transformations = transformation_json["children"][2]
-    del transformations["attributes"][0]["name"]
-    del transformations["attributes"][0]["values"]
+    del transformation_json["attributes"][0]["name"]
+    del transformation_json["attributes"][0]["values"]
 
-    assert not _contains_transformations(transformations)
+    assert not _contains_transformations(transformation_json)
 
 
 def test_GIVEN_no_attributes_field_in_dict_WHEN_checking_for_transformations_THEN_contains_transformations_returns_false(
     transformation_json,
 ):
+    del transformation_json["attributes"]
+    assert not _contains_transformations(transformation_json)
 
-    transformations = transformation_json["children"][2]
-    del transformations["attributes"]
 
-    assert not _contains_transformations(transformations)
+def test_GIVEN_property_not_found_WHEN_looking_for_transformation_property_THEN_get_transformation_property_returns_failure_value(
+    transformation_json, transformation_reader
+):
+    # record the current number of warnings in the TransformationReader
+    n_warnings = len(transformation_reader.warnings)
+
+    failure_value = 20
+    property_name = "DoesNotExist"
+    property_value = transformation_reader._get_transformation_property(
+        property_name, transformation_json["children"][0], failure_value
+    )
+
+    # Check that the failure value was returned
+    assert property_value == failure_value
+    # Check that the number of warnings has increased
+    assert len(transformation_reader.warnings) == n_warnings + 1
+    # Check that the latest warning mentions the name of the property that could not be found
+    assert property_name in transformation_reader.warnings[-1]
+
+
+def test_GIVEN_property_is_found_WHEN_looking_for_transformation_property_THEN_get_transformation_property_returns_property_value(
+    transformation_json, transformation_reader
+):
+    # record the current number of warnings in the TransformationReader
+    n_warnings = len(transformation_reader.warnings)
+    # Set the values property
+    transformation_json["children"][0]["values"] = json_value = 300
+
+    property_value = transformation_reader._get_transformation_property(
+        "values", transformation_json["children"][0], failure_value=50
+    )
+
+    # Check that the json value was returned
+    assert property_value == json_value
+    # Check that the number of warnings has remained the same
+    assert len(transformation_reader.warnings) == n_warnings
