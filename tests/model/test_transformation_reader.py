99610ead007515add037ebf28aae510d82b1bb6d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-06-24 16:00:06 +0100

    full spectrum of property in list tests

--- tests/model/test_transformation_reader.py
+++ tests/model/test_transformation_reader.py
@@ -65,6 +65,11 @@ def transformation_json():
 
 
 @pytest.fixture(scope="function")
+def attributes_list(transformation_json):
+    return transformation_json["children"][0]["attributes"]
+
+
+@pytest.fixture(scope="function")
 def transformation_reader(transformation_json):
     parent_component = Mock(spec=Component)
     parent_component.name = "ParentComponentName"
@@ -99,7 +104,6 @@ def test_GIVEN_no_attributes_field_in_dict_WHEN_checking_for_transformations_THE
 def test_GIVEN_property_not_found_WHEN_looking_for_transformation_property_THEN_get_transformation_property_returns_failure_value(
     transformation_json, transformation_reader
 ):
-    # Record the current number of warnings in the TransformationReader
     n_warnings = len(transformation_reader.warnings)
 
     failure_value = 20
@@ -119,8 +123,8 @@ def test_GIVEN_property_not_found_WHEN_looking_for_transformation_property_THEN_
 def test_GIVEN_property_is_found_WHEN_looking_for_transformation_property_THEN_get_transformation_property_returns_property_value(
     transformation_json, transformation_reader
 ):
-    # Record the current number of warnings in the TransformationReader
     n_warnings = len(transformation_reader.warnings)
+
     # Set the values property
     transformation_json["children"][0]["values"] = json_value = 300
 
@@ -135,20 +139,56 @@ def test_GIVEN_property_is_found_WHEN_looking_for_transformation_property_THEN_g
 
 
 def test_GIVEN_property_is_in_list_WHEN_looking_for_transformation_property_THEN_get_property_in_list_returns_true(
-    transformation_json, transformation_reader
+    transformation_reader, attributes_list
 ):
-    # Record the current number of warnings in the TransformationReader
     n_warnings = len(transformation_reader.warnings)
 
     # Set the units value
-    attributes_list = transformation_json["children"][0]["attributes"]
     attributes_list[0]["values"] = json_value = "cm"
 
     property_value = transformation_reader._find_property_in_list(
         "units", "TransformationName", attributes_list, failure_value="yards"
     )
 
-    # Check that json value was returned
+    # Check that the json value was returned
     assert property_value == json_value
     # Check that the number of warnings has remained the same
     assert len(transformation_reader.warnings) == n_warnings
+
+
+def test_GIVEN_property_name_not_in_list_WHEN_looking_for_transformation_property_THEN_get_property_in_list_returns_false(
+    transformation_reader, attributes_list
+):
+    n_warnings = len(transformation_reader.warnings)
+    failure_value = "yards"
+
+    # Remove the name field from the nested units dictionary
+    del attributes_list[0]["name"]
+
+    property_value = transformation_reader._find_property_in_list(
+        "units", "TransformationName", attributes_list, failure_value
+    )
+
+    # Check that the failure value was returned
+    assert property_value == failure_value
+    # Check that the number of warnings has increased
+    assert len(transformation_reader.warnings) == n_warnings + 1
+
+
+def test_GIVEN_property_value_not_in_list_WHEN_looking_for_transformation_property_THEN_get_property_in_list_returns_false(
+    transformation_reader, attributes_list
+):
+    n_warnings = len(transformation_reader.warnings)
+    failure_value = "yards"
+
+    # Remove the values field from the nested units dictionary
+    del attributes_list[0]["values"]
+
+    property_value = transformation_reader._find_property_in_list(
+        "units", "TransformationName", attributes_list, failure_value
+    )
+
+    # Check that the failure value was returned
+    assert property_value == failure_value
+    # Check that the number of warnings has increased
+    assert len(transformation_reader.warnings) == n_warnings + 1
