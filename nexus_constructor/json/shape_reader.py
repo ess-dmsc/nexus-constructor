a7a9b478f57cb6cedd08814864f630c6fbdfb24d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-07-02 12:06:27 +0100

    getting units information

--- nexus_constructor/json/shape_reader.py
+++ nexus_constructor/json/shape_reader.py
@@ -1,10 +1,21 @@
 from typing import List, Union
 
-from nexus_constructor.json.load_from_json_utils import _find_nx_class
+from PySide2.QtGui import QVector3D
+
+from nexus_constructor.json.load_from_json_utils import (
+    _find_nx_class,
+    _find_attribute_from_list_or_dict,
+)
 from nexus_constructor.model.component import (
     Component,
     CYLINDRICAL_GEOMETRY_NX_CLASS,
     OFF_GEOMETRY_NX_CLASS,
+)
+from nexus_constructor.unit_utils import (
+    units_are_recognised_by_pint,
+    METRES,
+    units_are_expected_dimensionality,
+    units_have_magnitude_of_one,
 )
 
 
@@ -37,7 +48,7 @@ class ShapeReader:
         try:
             children = self.shape_info["children"]
         except KeyError:
-            self.warnings.append("No children!")  # todo: proper message
+            self.warnings.append("No children!")  # todo: proper messages
             return
 
         faces_dataset = self._get_shape_attribute_from_list("faces", children)
@@ -48,12 +59,20 @@ class ShapeReader:
         if not vertices_dataset:
             return
 
-        winding_order = self._get_shape_attribute_from_list("winding_order", children)
-        if not winding_order:
+        winding_order_dataset = self._get_shape_attribute_from_list(
+            "winding_order", children
+        )
+        if not winding_order_dataset:
             return
 
-        faces = self._find_and_validate_faces_list(faces_dataset)
+        faces = self._find_and_validate_faces_list(
+            faces_dataset
+        )  # todo: format for OFFGeometry class?
         if not faces:
+            return
+
+        units = self._find_and_validate_units(vertices_dataset)
+        if not units:
             return
 
         vertices = self._find_and_validate_vertices(vertices_dataset)
@@ -79,16 +98,15 @@ class ShapeReader:
         self, faces_dataset: dict
     ) -> Union[List[int], None]:
 
-        try:
-            if "int" not in faces_dataset["dataset"]["type"]:
-                self.warnings.append("A message.")
-        except KeyError:
-            self.warnings.append("A message.")
-            pass
+        self._validate_data_type(faces_dataset, "int")
 
         try:
             faces = faces_dataset["values"]
         except KeyError:
+            self.warnings.append("A message.")
+            return
+
+        if not isinstance(faces, list):
             self.warnings.append("A message.")
             return
 
@@ -101,4 +119,58 @@ class ShapeReader:
     def _find_and_validate_vertices(
         self, vertices_dataset: dict
     ) -> Union[List[int], None]:
-        pass
+
+        self._validate_data_type(vertices_dataset, "float")
+
+        try:
+            values = vertices_dataset["values"]
+        except KeyError:
+            self.warnings.append("A message.")
+
+        if not isinstance(values, list):
+            self.warnings.append("A message.")
+            return
+
+        vertices = []
+
+        for value in values:
+            try:
+                vertices.append(QVector3D(*value))
+            except TypeError:
+                self.warnings.append("A message.")
+                return
+
+        return vertices
+
+    def _validate_data_type(self, dataset: dict, expected_type: str):
+
+        try:
+            if expected_type not in dataset["dataset"]["type"]:
+                self.warnings.append("A message.")
+        except KeyError:
+            self.warnings.append("A message.")
+
+    def _find_and_validate_units(self, vertices_dataset: dict) -> Union[str, None]:
+
+        try:
+            attributes_list = vertices_dataset["attributes"]
+        except KeyError:
+            self.warnings.append("A warnings.")
+            return
+
+        units = _find_attribute_from_list_or_dict("units", attributes_list)
+        if not units:
+            self.warnings.append("A warnings.")
+            return
+
+        if not units_are_recognised_by_pint(units, False):
+            self.warnings.append("A warnings.")
+            return
+        if not units_are_expected_dimensionality(units, METRES, False):
+            self.warnings.append("A warnings.")
+            return
+        if not units_have_magnitude_of_one(units, False):
+            self.warnings.append("A warnings.")
+            return
+
+        return units
