6a995dbca33b26139dab296f4446de1144f0f7ad
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-07-03 16:03:14 +0100

    name not found test

--- tests/test_shape_reader.py
+++ tests/test_shape_reader.py
@@ -36,7 +36,7 @@ def off_shape_reader(off_shape_json, mock_component) -> ShapeReader:
     return ShapeReader(mock_component, off_shape_json)
 
 
-def test_GIVEN_off_shape_WHEN_reading_shape_information_THEN_error_and_warning_messages_have_expected_content(
+def test_GIVEN_off_shape_WHEN_reading_shape_information_THEN_error_and_issue_messages_have_expected_content(
     off_shape_reader,
 ):
     off_shape_reader.add_shape_to_component()
@@ -44,3 +44,40 @@ def test_GIVEN_off_shape_WHEN_reading_shape_information_THEN_error_and_warning_m
     for message in [off_shape_reader.error_message, off_shape_reader.issue_message]:
         assert OFF_GEOMETRY_NX_CLASS in message
         assert OFF_GEOMETRY_NX_CLASS in message
+
+
+def test_GIVEN_unrecognised_shape_WHEN_reading_shape_information_THEN_warning_message_is_created(
+    off_shape_reader, off_shape_json
+):
+    n_warnings = len(off_shape_reader.warnings)
+
+    off_shape_json["attributes"][0]["values"] = bad_geometry_type = "NotAValidGeometry"
+    off_shape_reader.add_shape_to_component()
+
+    assert len(off_shape_reader.warnings) == n_warnings + 1
+    assert bad_geometry_type in off_shape_reader.warnings[-1]
+
+
+def test_GIVEN_missing_children_attribute_WHEN_reading_off_information_THEN_warning_message_is_created(
+    off_shape_reader, off_shape_json
+):
+    n_warnings = len(off_shape_reader.warnings)
+
+    del off_shape_json["children"]
+    off_shape_reader.add_shape_to_component()
+
+    assert len(off_shape_reader.warnings) == n_warnings + 1
+    assert "Unable to find children" in off_shape_reader.warnings[-1]
+
+
+def test_GIVEN_missing_name_WHEN_reading_off_information_THEN_warning_message_is_created_and_substitute_name_is_used(
+    off_shape_reader, off_shape_json, mock_component
+):
+
+    n_warnings = len(off_shape_reader.warnings)
+
+    del off_shape_json["name"]
+    off_shape_reader.add_shape_to_component()
+
+    assert len(off_shape_reader.warnings) == n_warnings + 1
+    assert mock_component["shape"].name == "shape"
