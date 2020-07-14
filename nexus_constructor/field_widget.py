3d2edea666bfe80fa165f7e08fa33c6d0aa42459
Jack Harper <jack.harper@stfc.ac.uk>
2020-07-14 16:13:38 +0100

    Fix parent setting for field widget (fixes UI tests)

--- nexus_constructor/field_widget.py
+++ nexus_constructor/field_widget.py
@@ -86,9 +86,10 @@ class FieldWidget(QFrame):
             possible_field_names = []
 
         self.edit_dialog = QDialog(parent=self)
-        self.parent().parent().destroyed.connect(self.edit_dialog.close)
         self.attrs_dialog = FieldAttrsDialog(parent=self)
-        self.parent().parent().destroyed.connect(self.attrs_dialog.close)
+        if self.parent() is not None and self.parent().parent() is not None:
+            self.parent().parent().destroyed.connect(self.edit_dialog.close)
+            self.parent().parent().destroyed.connect(self.attrs_dialog.close)
 
         self.field_name_edit = FieldNameLineEdit(possible_field_names)
         self.hide_name_field = hide_name_field
