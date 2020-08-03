df3c9eb1ed3342d16ce7e8a6947bb6d35faf4589
Matthew D Jones <matthew.d.jones@tessella.com>
2020-08-03 15:50:07 +0100

    Don't populate null values in JSON for option stream fields

--- nexus_constructor/model/stream.py
+++ nexus_constructor/model/stream.py
@@ -92,19 +92,25 @@ class EV42Stream:
     nexus_chunk_chunk_kb = attr.ib(type=int, default=None)
 
     def as_dict(self):
-        return {
+        dict = {
             CommonKeys.TYPE: NodeType.STREAM,
             NodeType.STREAM: {
                 WRITER_MODULE: self.writer_module,
                 SOURCE: self.source,
                 TOPIC: self.topic,
-                ADC_PULSE_DEBUG: self.adc_pulse_debug if not None else {},
-                INDEX_EVERY_MB: self.nexus_indices_index_every_mb if not None else {},
-                INDEX_EVERY_KB: self.nexus_indices_index_every_kb if not None else {},
-                CHUNK_CHUNK_MB: self.nexus_chunk_chunk_mb if not None else {},
-                CHUNK_CHUNK_KB: self.nexus_chunk_chunk_kb if not None else {},
             },
         }
+        if self.adc_pulse_debug is not None:
+            dict[ADC_PULSE_DEBUG] = self.adc_pulse_debug
+        if self.nexus_indices_index_every_mb is not None:
+            dict[INDEX_EVERY_MB] = self.nexus_indices_index_every_mb
+        if self.nexus_indices_index_every_kb is not None:
+            dict[INDEX_EVERY_KB] = self.nexus_indices_index_every_kb
+        if self.nexus_chunk_chunk_mb is not None:
+            dict[CHUNK_CHUNK_MB] = self.nexus_chunk_chunk_mb
+        if self.nexus_chunk_chunk_kb is not None:
+            dict[CHUNK_CHUNK_KB] = self.nexus_chunk_chunk_kb
+        return dict
 
 
 @attr.s
@@ -120,20 +126,26 @@ class F142Stream:
     store_latest_into = attr.ib(type=int, default=None)
 
     def as_dict(self):
-        return {
+        dict = {
             CommonKeys.TYPE: NodeType.STREAM,
             NodeType.STREAM: {
                 WRITER_MODULE: self.writer_module,
                 SOURCE: self.source,
                 TOPIC: self.topic,
                 VALUE_UNITS: self.value_units,
-                CommonKeys.TYPE: self.type if not None else {},
-                ARRAY_SIZE: self.array_size if not None else {},
-                INDEX_EVERY_MB: self.nexus_indices_index_every_mb if not None else {},
-                INDEX_EVERY_KB: self.nexus_indices_index_every_kb if not None else {},
-                STORE_LATEST_INTO: self.store_latest_into if not None else {},
             },
         }
+        if self.type is not None:
+            dict[CommonKeys.TYPE] = self.type
+        if self.array_size is not None:
+            dict[ARRAY_SIZE] = self.array_size
+        if self.nexus_indices_index_every_mb is not None:
+            dict[INDEX_EVERY_MB] = self.nexus_indices_index_every_mb
+        if self.nexus_indices_index_every_kb is not None:
+            dict[INDEX_EVERY_KB] = self.nexus_indices_index_every_kb
+        if self.store_latest_into is not None:
+            dict[STORE_LATEST_INTO] = self.store_latest_into
+        return dict
 
 
 HS00TYPES = ["uint32", "uint64", "float", "double"]
