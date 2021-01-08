95d8e53664427a340b003b8f3b6eb87009d9b830
Matthew D Jones <matthew.d.jones@tessella.com>
2021-01-08 09:03:24 +0000

    in comment explain why TransformId is made hashable

--- nexus_constructor/json/transform_id.py
+++ nexus_constructor/json/transform_id.py
@@ -1,7 +1,7 @@
 import attr
 
 
-# frozen=True makes objects hashable
+# frozen=True makes objects hashable (for use as dictionary key)
 @attr.s(frozen=True)
 class TransformId:
     component_name = attr.ib(type=str)
