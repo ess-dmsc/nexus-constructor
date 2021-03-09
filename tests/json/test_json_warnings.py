7648026acbfab3e8d518cba9613051cf937ee9f4
Kenan Muric <kenan.muric@ess.eu>
2021-03-09 23:45:56 +0100

    Adding new container class to better control how json warnings are handled in the code base

---
+++ tests/json/test_json_warnings.py
@@ -0,0 +1,49 @@
+import pytest
+
+from nexus_constructor.json.json_warnings import InvalidJson, JsonWarningsContainer
+
+JSON_WARN_CONTAINER = JsonWarningsContainer()
+JSON_WARNING = InvalidJson("Invalid JSON warning")
+JSON_WARN_CONTAINER.append(JSON_WARNING)
+INVALID_CONTAINER_ELEMENT = "INVALID"
+
+
+def test_json_warnings_container_raises_type_error_when_instantiating_with_wrong_type():
+    with pytest.raises(TypeError):
+        JsonWarningsContainer([INVALID_CONTAINER_ELEMENT])
+
+
+def test_json_warning_container_is_correctly_instantiated_when_providing_correct_other_container():
+    this_container = JsonWarningsContainer(JSON_WARN_CONTAINER)
+    assert this_container
+
+
+def test_json_warning_container_is_correctly_instantiated_when_providing_warning():
+    this_container = JsonWarningsContainer(JSON_WARNING)
+    assert this_container
+
+
+def test_json_warning_container_raises_type_error_if_appending_item_of_invalid_type():
+    with pytest.raises(TypeError):
+        JSON_WARN_CONTAINER.append(INVALID_CONTAINER_ELEMENT)
+
+
+def test_json_warning_container_when_using_add_operator_with_correct_type():
+    assert len(JSON_WARN_CONTAINER + JSON_WARNING + JSON_WARNING) == 3
+
+
+def test_json_warning_container_when_using_iadd_operator_with_correct_type():
+    this_container = JsonWarningsContainer(JSON_WARN_CONTAINER)
+    this_container += JSON_WARNING
+    assert len(this_container) == 2
+
+
+def test_json_warning_container_when_using_add_operator_with_incorrect_type():
+    with pytest.raises(TypeError):
+        JSON_WARN_CONTAINER + INVALID_CONTAINER_ELEMENT
+
+
+def test_json_warning_container_when_using_iadd_operator_with_incorrect_type():
+    with pytest.raises(TypeError):
+        this_container = JsonWarningsContainer(JSON_WARN_CONTAINER)
+        this_container += INVALID_CONTAINER_ELEMENT
