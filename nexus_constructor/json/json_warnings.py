176b0f411dccaca9c6e19007ee0a5d8115f0185b
Kenan Muric <kenan.muric@ess.eu>
2021-03-10 09:43:35 +0100

    Refactoring and documenting json warnings container class. Unit test for load json warning should now break when adding strings to warnings container

--- nexus_constructor/json/json_warnings.py
+++ nexus_constructor/json/json_warnings.py
@@ -8,45 +8,6 @@ The warning messages are wrapped in these classes to facilitate testing that
 the expected warning is triggered without having to test exact warning string
 """
 
-_T = TypeVar("JsonWarning")
-
-
-class JsonWarningsContainer(List[_T]):
-    def __init__(self, *args, **kwargs):
-        self._CHECK_TYPES = (JsonWarning.__args__, JsonWarningsContainer)
-        self.__add_constructor_param_items(args)
-        self.__add_constructor_param_items(kwargs)
-
-    def __add_constructor_param_items(self, other_items):
-        if other_items:
-            if not any(isinstance(item, self._CHECK_TYPES) for item in other_items):
-                raise TypeError(
-                    "The provided parameter to the constructor "
-                    "is not of correct type"
-                )
-            else:
-                self.extend(other_items)
-
-    def append(self, __object: _T) -> None:
-        if __object and isinstance(__object, self._CHECK_TYPES):
-            super().append(__object)
-        else:
-            raise TypeError(f"Item can only be appended if it is of type {_T}")
-
-    def insert(self, __index: int, __object: _T) -> None:
-        if __object and isinstance(__object, self._CHECK_TYPES):
-            super().insert(__index, __object)
-        else:
-            raise TypeError(f"Item can only be appended if it is of type {_T}")
-
-    def __add__(self, __object: _T) -> None:
-        self.append(__object)
-        return self
-
-    def __iadd__(self, __object: _T) -> None:
-        self.append(__object)
-        return self
-
 
 @attr.s
 class InvalidJson:
@@ -71,3 +32,51 @@ class NXClassAttributeMissing:
 JsonWarning = Union[
     InvalidJson, TransformDependencyMissing, NameFieldMissing, NXClassAttributeMissing
 ]
+
+_T = TypeVar("_T", JsonWarning, "JsonWarningsContainer")
+
+
+class JsonWarningsContainer(List[JsonWarning]):
+    """
+    This is a container class that should only store items of types given in JsonWarning.
+    It does the necessary type checks before inserting new items into the container.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self._CHECK_TYPES = (JsonWarning.__args__, JsonWarningsContainer)
+        self.__add_constructor_param_items(args)
+        self.__add_constructor_param_items(kwargs)
+
+    def __add_constructor_param_items(self, other_items):
+        if other_items:
+            if not any(isinstance(item, self._CHECK_TYPES) for item in other_items):
+                raise TypeError(
+                    "The provided parameter to the constructor "
+                    "is not of correct type"
+                )
+            else:
+                self.extend(other_items)
+
+    def append(self, __object: _T) -> None:
+        if __object and isinstance(__object, self._CHECK_TYPES):
+            super().append(__object)
+        else:
+            raise TypeError(
+                f"Item can only be appended if it is of type in {JsonWarning}"
+            )
+
+    def insert(self, __index: int, __object: _T) -> None:
+        if __object and isinstance(__object, self._CHECK_TYPES):
+            super().insert(__index, __object)
+        else:
+            raise TypeError(
+                f"Item can only be appended if it is of type in {JsonWarning}"
+            )
+
+    def __add__(self, __object: _T) -> None:
+        self.append(__object)
+        return self
+
+    def __iadd__(self, __object: _T) -> None:
+        self.append(__object)
+        return self
