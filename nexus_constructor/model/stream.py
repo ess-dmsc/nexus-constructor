79871c6d9069978b29929fbd4fdd1cf6df8948cd
Kenan Muric <kenan.muric@ess.eu>
2021-05-10 10:13:23 +0200

    Fixing mypy error in function as_dict in class EV42Stream in stream.py

--- nexus_constructor/model/stream.py
+++ nexus_constructor/model/stream.py
@@ -37,10 +37,7 @@ class NS10Stream:
     def as_dict(self):
         return {
             CommonKeys.MODULE: self.writer_module,
-            NodeType.CONFIG: {
-                SOURCE: self.source,
-                TOPIC: self.topic,
-            },
+            NodeType.CONFIG: {SOURCE: self.source, TOPIC: self.topic},
         }
 
 
@@ -53,10 +50,7 @@ class SENVStream:
     def as_dict(self):
         return {
             CommonKeys.MODULE: self.writer_module,
-            NodeType.CONFIG: {
-                SOURCE: self.source,
-                TOPIC: self.topic,
-            },
+            NodeType.CONFIG: {SOURCE: self.source, TOPIC: self.topic},
         }
 
 
@@ -69,10 +63,7 @@ class TDCTStream:
     def as_dict(self):
         return {
             CommonKeys.MODULE: self.writer_module,
-            NodeType.CONFIG: {
-                SOURCE: self.source,
-                TOPIC: self.topic,
-            },
+            NodeType.CONFIG: {SOURCE: self.source, TOPIC: self.topic},
         }
 
 
@@ -88,24 +79,25 @@ class EV42Stream:
     nexus_chunk_chunk_kb = attr.ib(type=int, default=None)
 
     def as_dict(self):
-        dict = {
+        config_dict: dict = {
             CommonKeys.MODULE: self.writer_module,
-            NodeType.CONFIG: {
-                SOURCE: self.source,
-                TOPIC: self.topic,
-            },
+            NodeType.CONFIG: {SOURCE: self.source, TOPIC: self.topic},
         }
         if self.adc_pulse_debug is not None:
-            dict[NodeType.CONFIG][ADC_PULSE_DEBUG] = self.adc_pulse_debug  # type: ignore
+            config_dict[NodeType.CONFIG][ADC_PULSE_DEBUG] = self.adc_pulse_debug
         if self.nexus_indices_index_every_mb is not None:
-            dict[NodeType.CONFIG][INDEX_EVERY_MB] = self.nexus_indices_index_every_mb  # type: ignore
+            config_dict[NodeType.CONFIG][
+                INDEX_EVERY_MB
+            ] = self.nexus_indices_index_every_mb
         if self.nexus_indices_index_every_kb is not None:
-            dict[NodeType.CONFIG][INDEX_EVERY_KB] = self.nexus_indices_index_every_kb  # type: ignore
+            config_dict[NodeType.CONFIG][
+                INDEX_EVERY_KB
+            ] = self.nexus_indices_index_every_kb
         if self.nexus_chunk_chunk_mb is not None:
-            dict[NodeType.CONFIG][CHUNK_CHUNK_MB] = self.nexus_chunk_chunk_mb  # type: ignore
+            config_dict[NodeType.CONFIG][CHUNK_CHUNK_MB] = self.nexus_chunk_chunk_mb
         if self.nexus_chunk_chunk_kb is not None:
-            dict[NodeType.CONFIG][CHUNK_CHUNK_KB] = self.nexus_chunk_chunk_kb  # type: ignore
-        return dict
+            config_dict[NodeType.CONFIG][CHUNK_CHUNK_KB] = self.nexus_chunk_chunk_kb
+        return config_dict
 
 
 @attr.s
@@ -186,9 +178,7 @@ class StreamGroup(Group):
     children: List[Stream] = attr.ib(factory=list, init=False)  # type: ignore
 
     def __setitem__(  # type: ignore
-        self,
-        key: str,
-        value: Stream,
+        self, key: str, value: Stream,
     ):
         self.children.append(value)
 
