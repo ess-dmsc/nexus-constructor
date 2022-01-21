e2a888ca6ea7c1d3f8813652c8e1c983dfcc85e4
Ebad Kamil <ebad.kamil@ess.eu>
2022-01-21 14:57:07 +0100

    Add test size of tree structure

--- tests/model/test_group.py
+++ tests/model/test_group.py
@@ -5,6 +5,26 @@ from nexus_constructor.model.group import Group
 from nexus_constructor.model.module import Dataset
 
 
+def create_tree_structure():
+    entry = Group("entry")
+    sample = Component("sample", parent_node=entry)
+    instrument = Group("instrument", parent_node=entry)
+    g1 = Group("g1", parent_node=instrument)
+    g2 = Group("g2", parent_node=instrument)
+    g21 = Group("g21", parent_node=g2)
+
+    c1 = Component("c1", parent_node=instrument)
+    c2 = Group("c2", parent_node=instrument)
+
+    dataset = Dataset(name="dataset", values=0, parent_node=sample)
+    entry.children = [sample, instrument]
+    sample.children = [g1, g2, dataset]
+    g2.children = [g21]
+    instrument.children = [c1, c2]
+
+    return entry, sample, instrument
+
+
 def test_get_field_value_throws_if_field_does_not_exist():
     group = Group("test_group")
 
@@ -27,22 +47,15 @@ def test_group_as_dict_contains_expected_keys():
 
 
 def test_group_depth_returns_expected_tree_depth():
-    entry = Group("entry")
-    sample = Component("sample", parent_node=entry)
-    instrument = Group("instrument", parent_node=entry)
-    g1 = Group("g1", parent_node=instrument)
-    g2 = Group("g2", parent_node=instrument)
-    g21 = Group("g21", parent_node=g2)
-
-    c1 = Component("c1", parent_node=instrument)
-    c2 = Group("c2", parent_node=instrument)
-
-    dataset = Dataset(name="dataset", values=0, parent_node=sample)
-    entry.children = [sample, instrument]
-    sample.children = [g1, g2, dataset]
-    g2.children = [g21]
-    instrument.children = [c1, c2]
+    entry, sample, instrument = create_tree_structure()
 
     assert entry.tree_depth() == 4
     assert sample.tree_depth() == 3
     assert instrument.tree_depth() == 2
+
+
+def test_group_size_returns_expected_tree_size():
+    entry, sample, instrument = create_tree_structure()
+    assert entry.tree_size() == 9
+    assert sample.tree_size() == 5
+    assert instrument.tree_size() == 3
