fdadca39b53902c9259191d197ef625f24ccb9d0
Ebad Kamil <ebad.kamil@ess.eu>
2022-01-21 14:28:33 +0100

    Add unittest for tree depth in group

--- tests/model/test_group.py
+++ tests/model/test_group.py
@@ -1,5 +1,6 @@
 import pytest
 
+from nexus_constructor.model.component import Component
 from nexus_constructor.model.group import Group
 
 
@@ -22,3 +23,24 @@ def test_group_as_dict_contains_expected_keys():
         assert expected_key in dictionary_output.keys()
 
     assert dictionary_output["name"] == input_name
+
+
+def test_group_depth_returns_expected_tree_depth():
+    entry = Group("entry")
+    sample = Component("sample", parent_node=entry)
+    instrument = Group("instrument", parent_node=entry)
+    g1 = Group("g1", parent_node=instrument)
+    g2 = Group("g2", parent_node=instrument)
+    g21 = Group("g21", parent_node=g2)
+
+    c1 = Component("c1", parent_node=instrument)
+    c2 = Group("c2", parent_node=instrument)
+
+    entry.children = [sample, instrument]
+    sample.children = [g1, g2]
+    g2.children = [g21]
+    instrument.children = [c1, c2]
+
+    assert entry.tree_depth() == 4
+    assert sample.tree_depth() == 3
+    assert instrument.tree_depth() == 2
