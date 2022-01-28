52203d4c910e2f8f49dfb38024a8ff7d46467153
Kenan Muric <kenan.muric@ess.eu>
2022-01-28 13:59:39 +0100

    fixing handling of units

--- nexus_constructor/module_view.py
+++ nexus_constructor/module_view.py
@@ -1,5 +1,6 @@
 from PySide2.QtWidgets import QGroupBox, QVBoxLayout, QWidget
 
+from nexus_constructor.common_attrs import CommonAttrs
 from nexus_constructor.field_utils import find_field_type
 from nexus_constructor.field_widget import FieldWidget
 from nexus_constructor.model.model import Model
@@ -16,19 +17,26 @@ class ModuleView(QGroupBox):
         self.setLayout(layout)
         self.module = module
         self.model = model
-        self._set_existing_items(module)
+        self._set_existing_items()
 
-    def _set_existing_items(self, module):
+    def _set_existing_items(self):
+        module = self.module
         update_function = find_field_type(module, [])
         if update_function is not None:
             update_function(module, self.field_widget)
         self.field_widget.name = module.name
         if hasattr(module, "type") and not module.type:
             self.field_widget.dtype = ValueTypes.STRING
+        units = module.attributes.get_attribute_value(CommonAttrs.UNITS)
+        if units:
+            self.field_widget.units = units
 
     def save_module_changes(self):
         if self.module.writer_module == WriterModules.DATASET.value:
             self.module.name = self.field_widget.name
             self.module.values = self.field_widget.value
             self.module.type = self.field_widget.dtype
+            self.module.attributes.set_attribute_value(
+                CommonAttrs.UNITS, self.field_widget.units
+            )
         self.model.signals.module_changed.emit()
