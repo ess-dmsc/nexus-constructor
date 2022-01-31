1faa701b14bab90bbf9b51d4a1ba2d12b5b32a28
Ebad Kamil <ebad.kamil@ess.eu>
2022-01-31 11:10:35 +0100

    Rfeactoring find field

--- nexus_constructor/module_view.py
+++ nexus_constructor/module_view.py
@@ -1,11 +1,10 @@
 from PySide2.QtWidgets import QGroupBox, QVBoxLayout, QWidget
 
-from nexus_constructor.common_attrs import CommonAttrs, CommonKeys
+from nexus_constructor.common_attrs import CommonAttrs
 from nexus_constructor.field_utils import find_field_type
 from nexus_constructor.field_widget import FieldWidget
 from nexus_constructor.model.model import Model
 from nexus_constructor.model.module import WriterModules
-from nexus_constructor.model.value_type import ValueTypes
 
 
 class ModuleView(QGroupBox):
@@ -21,23 +20,16 @@ class ModuleView(QGroupBox):
 
     def _set_existing_items(self):
         module = self.module
-        update_function = find_field_type(module, [])
         if self.module.writer_module == WriterModules.DATASET.value:
+            update_function = find_field_type(module, [])
             if update_function is not None:
                 update_function(module, self.field_widget)
-            self.field_widget.name = module.name
-            if hasattr(module, CommonKeys.TYPE) and not module.type:
-                self.field_widget.dtype = ValueTypes.STRING
-            units = module.attributes.get_attribute_value(CommonAttrs.UNITS)
         elif self.module.writer_module == WriterModules.LINK.value:
             pass
         else:
+            update_function = find_field_type(module.parent_node, [])
             if update_function is not None:
                 update_function(module.parent_node, self.field_widget)
-            self.field_widget.name = module.parent_node.name
-            units = module.parent_node.attributes.get_attribute_value(CommonAttrs.UNITS)
-        if units:
-            self.field_widget.units = units
 
     def save_module_changes(self):
         if self.module.writer_module == WriterModules.DATASET.value:
