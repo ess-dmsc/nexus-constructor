61746744ead12f159852cf69135bf0dab298adf2
Kenan Muric <kenan.muric@ess.eu>
2022-05-02 16:51:46 +0200

    removing unneccesary code

--- nexus_constructor/unique_name.py
+++ nexus_constructor/unique_name.py
@@ -10,11 +10,8 @@ def generate_unique_name(base: str, items: list):
     """
     regex = f"^{re.escape(base)}\\d*$"
     similar_names = [item.name for item in items if re.match(regex, item.name)]
-
     if len(similar_names) == 0 or base not in similar_names:
         return base
-    if similar_names == [base]:
-        return base + "1"
     # find the highest number in use, and go one higher
     tailing_numbers = [int(name[len(base) :]) for name in similar_names if name != base]
     return base + str(max(tailing_numbers) + 1) if tailing_numbers else base + "1"
