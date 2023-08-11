89cd557fd9888de192e0da075ca8f641aaffabbb
George O'Neill <george.oneill@ess.eu>
2023-08-11 18:35:22 +0200

    typo in addition of name and parent_node to module.py

--- nexus_constructor/model/module.py
+++ nexus_constructor/model/module.py
@@ -57,8 +57,9 @@ class StreamModules(Enum):
 
 @attr.s
 class FileWriterModule(ABC):
-    name: str = None
-    parent_node: str = None    attributes = attr.ib(type=Attributes, factory=Attributes, init=False)
+    name = ""
+    parent_node = ""
+    attributes = attr.ib(type=Attributes, factory=Attributes, init=False)
     writer_module = attr.ib(type=str, init=False)
     parent_node = attr.ib(type="Group")
 
