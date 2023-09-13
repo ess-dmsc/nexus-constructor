976ab1140b39081113bcd8992e0c888ee73ef6ab
George O'Neill <george.oneill@ess.eu>
2023-09-13 14:39:58 +0200

    Adjusted so depends_on of len 1 are read correctly

--- nexus_constructor/json/load_from_json.py
+++ nexus_constructor/json/load_from_json.py
@@ -401,8 +401,8 @@ class JSONReader:
         if depends_on not in DEPENDS_ON_IGNORE:
             if depends_on[0] != "/":
                 #   we are always in the NXtransformations group but the path could be anything
-                if len(depends_on.split("/")) == 2:
-                    depends_on = "transformations/" + depends_on.split("/")[1]
+                if len(depends_on.split("/")) <= 2:
+                    depends_on = "transformations/" + depends_on.split("/")[-1]
                 depends_on = component.absolute_path + depends_on
             depends_on_id = TransformId(*get_component_and_transform_name(depends_on))
             self._components_depends_on[component.name] = (component, depends_on_id)
