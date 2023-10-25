3b2dac6ac32633620a98dcde3e1944b66f51a169
George O'Neill <george.oneill@ess.eu>
2023-10-25 18:11:49 +0200

    removal of description and tests # altered

--- ui/parameters_widget.py
+++ ui/parameters_widget.py
@@ -10,3 +10,6 @@ class ParametersView(QWidget):
         self.model = None
         self.setLayout(QVBoxLayout())
         self.setParent(parent)
+        self.model.entry.proposal_id = ("", True)
+        self.model.entry.title = ("", True)
+        self.model.entry.users_placeholder = True
